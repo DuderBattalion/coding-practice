c396545fffc6cdf979ca77626177fc7ca9803430
Anurag Patnaik <anurag.patnaik@autodesk.com>
2020-04-03 16:34:33 -0700

    [SpiralArray] Refactoring code to increase readability. Added documentation.

--- CTCI/src/com/misc/spiralarray/PrintSpiralArray.java
+++ CTCI/src/com/misc/spiralarray/PrintSpiralArray.java
@@ -16,6 +16,20 @@ public class PrintSpiralArray {
         .substring(0, result.length() - RESULT_SEP.length()); // Exclude trailing separator
   }
 
+  /**
+   * Recurses through the 2D array doing 4 operations on repeat until it runs out
+   * of operations.
+   * Operation 1: Print N chars in row to the right
+   * Operation 2: From there, print N- 1 chars in the column downwards
+   * Operation 3: Print N - 1 chars in row to the left
+   * Operation 4: Print N - 2 chars in the column upwards
+   *
+   * Repeating this pattern over and over again will complete the spiral. The
+   * recursion terminates once all characters in a step have been printed.
+   * @param startRow Starting row position for recursion pass
+   * @param startCol Starting column position
+   * @param numChars Number of characters expected to be printed in Operation 1
+   */
   private void recurseSpiralArray(int startRow, int startCol,
                                   int numChars) {
     ArrayPosition nextPosition = printRowRight(startRow, startCol, numChars);
@@ -28,6 +42,15 @@ public class PrintSpiralArray {
     }
   }
 
+  // Note: the printRow<Direction>() functions below can be further optimized into a single printChars()
+  // function to eliminate some code redundancy, but with a trade-off having reduced readability. The
+  // code as such is easier to read and reduces complexity with the minor trade-off of some extra lines
+  // of code. This cab a good point of architectural discussion in a Pull Request with the team.
+
+  /**
+   * Prints a row, going right.
+   * @return Returns the next array position to print.
+   */
   private ArrayPosition printRowRight(int row, int col, int numChars) {
     if (numChars <= 0) {
       return null; // End recursion
