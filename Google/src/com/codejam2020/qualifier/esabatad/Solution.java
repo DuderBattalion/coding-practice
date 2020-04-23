53ac924fa0e0bb9b3d2b50ace782cefbae3a2cab
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-04-22 21:50:14 -0700

    Starting to get the interactive_runner running.
    Testing with test case 0 - B=10

--- Google/src/com/codejam2020/qualifier/esabatad/Solution.java
+++ Google/src/com/codejam2020/qualifier/esabatad/Solution.java
@@ -1,7 +1,10 @@
 package com.codejam2020.qualifier.esabatad;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,29 +13,57 @@ public class Solution {
   public static void main(String[] args) {
     Solution solution = new Solution();
 
+    // Debug messages into a file
+    FileWriter fileWriter = null;
+    try {
+      fileWriter = new FileWriter("DebugLog.txt");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    PrintWriter writer = new PrintWriter(fileWriter);
+
     Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
     int t = in.nextInt();
     int b = in.nextInt();
+    in.nextLine();
 
     int[] result = new int[b];
-    Arrays.fill(result, -1); // Default uninitialized value
+    for (int i = 0; i < t; i++) {
+      Arrays.fill(result, -1); // Default uninitialized value
 
-    int position = 0;
-    printLn(position + 1); // Position is 1 indexed, instead of 0 (as is for arrays)
-    flushStdOut();
-
-    result[position] = in.nextInt();
-    position++;
-
-    for (int i = position; i < b; i++) {
-      printLn(i + 1);
+      int position = 0;
+      printLn(position + 1); // Position is 1 indexed, instead of 0 (as is for arrays)
       flushStdOut();
 
-      result[i] = in.nextInt();
+      result[position] = in.nextInt();
+      in.nextLine();
+      writer.print(result[position]); // DEBUG
+      position++;
+      ;
+
+      for (int j = position; j < b; j++) {
+        printLn(j + 1);
+        flushStdOut();
+
+        result[j] = in.nextInt();
+        in.nextLine();
+        writer.print(result[j]); // DEBUG
+      }
+
+      printArr(result);
+      flushStdOut();
+
+      String judgeSays = in.nextLine();
+      if (judgeSays.equals("N")) {
+        break;
+      }
+
+      writer.println();
     }
 
-    printArr(result);
+    writer.close();
   }
 
   private static<T> void printLn(T msg) {
@@ -51,5 +82,7 @@ public class Solution {
     for (int bit: bits) {
       print(bit);
     }
+
+    printLn("");
   }
 }
