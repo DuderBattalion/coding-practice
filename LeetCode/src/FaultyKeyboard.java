a759f5ecff523a2d8694a5b509ebbd0a999a2b61
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-05-28 14:46:36 -0700

    [LeetCode] Partial solution to FaultyKeyboard.

--- LeetCode/src/FaultyKeyboard.java
+++ LeetCode/src/FaultyKeyboard.java
@@ -20,40 +20,46 @@ public class FaultyKeyboard {
     Set<String> dict = new HashSet<>();
     initDict(dict);
 
-    String s = "I lik   to   xplor   univ rs ";
-    SuffixArray validWordSuffs = createSuffArrFromDict(dict);
+    // String s = "I lik   to   xplor   univ rs ";
+    String s = "I lik  to  xplor  univ rs ";
+    // SuffixArray validWordSuffs = createSuffArrFromDict(dict);
 
-    printValidSentences("", "", 0, s, validWordSuffs);
+    printValidSentences("", "", 0, s, dict);
   }
 
+  // TODO - Performance enhancement. Instead of set, store in hashtable
+  // Key: first char, Val: String
+  // That way, we can check prefixes by only getting firstChar matching words
   private static void initDict(Set<String> dict) {
+    dict.add("I");
     dict.add("like");
-    dict.add("explore");
+    dict.add("to");
     dict.add("toe");
+    dict.add("explore");
     dict.add("universe");
     dict.add("rse");
   }
 
-  private static SuffixArray createSuffArrFromDict(Set<String> dict) {
-    List<String> sentinels = Arrays.asList("!", "@", "#", "$", "%", "^", "&");
-
-    StringBuilder buf = new StringBuilder();
-    Iterator<String> dictIter = dict.iterator();
-    String word;
-
-    int i = 0;
-    while (dictIter.hasNext()) {
-      word = dictIter.next();
-      buf.append(word);
-      buf.append(sentinels.get(i));
-      i++;
-    }
-
-    return new SuffixArray(buf.toString());
-  }
+  // private static SuffixArray createSuffArrFromDict(Set<String> dict) {
+  //   List<String> sentinels = Arrays.asList("!", "@", "#", "$", "%", "^", "&");
+  //
+  //   StringBuilder buf = new StringBuilder();
+  //   Iterator<String> dictIter = dict.iterator();
+  //   String word;
+  //
+  //   int i = 0;
+  //   while (dictIter.hasNext()) {
+  //     word = dictIter.next();
+  //     buf.append(word);
+  //     buf.append(sentinels.get(i));
+  //     i++;
+  //   }
+  //
+  //   return new SuffixArray(buf.toString());
+  // }
 
   private static void printValidSentences(String sentence, String currWord, int i,
-                                          String s, SuffixArray suffs) {
+                                          String s, Set<String> dict) {
     if (i == s.length()) {
       System.out.println(sentence);
       return;
@@ -62,25 +68,37 @@ public class FaultyKeyboard {
     NextWord nextWord = getNextWord(s, i);
 
     // NextWord index can be ' ' or 'e'
-    // If ' ', then currWord + nextWord needs to be a valid word
-    // If 'e', then recurse over currWord + nextWord + 'e'
+    // ' ' means current word is complete
+    // 'e' means current word is a prefix
 
-    // Case ' '
-    // If currWord + nextWord is a valid prefix, keep going, Else, return.
-    nextWord.word = currWord + nextWord.word;
-    if (isValidPrefix(nextWord.word, suffs)) {
-      printValidSentences(sentence + nextWord.word, "",
-          nextWord.index + 1, s, suffs);
-    } else {
-      return;
+    // If ' ', then check i reset current word and keep recursing.
+    // If 'e', then check if currWord + nextWord + 'e' is a valid prefix. If so,
+    // then keep going, else return.
+
+    // Case ' ' - Word complete
+    // Check if current word exists in the dict.
+    // If so, reset currentWord and continue, else return.
+    String completeWord = currWord + nextWord.word;
+    if (dict.contains(completeWord)) {
+      printValidSentences(sentence + completeWord + " ", "",
+            nextWord.index + 1, s, dict);
     }
 
-    // Case 'e'
-    printValidSentences(sentence + currWord + nextWord.word + 'e',
-        nextWord.word + 'e', i + 1, s, suffs);
+    // Case 'e' - Word prefix
+    // If prefix is valid, then continue, else return
+    String nextWordPrefix = currWord + nextWord.word + "e";
+    if (isValidPrefix(nextWordPrefix, dict)) {
+      printValidSentences(sentence,
+          nextWordPrefix, nextWord.index + 1, s, dict);
+    }
   }
 
 
+  /**
+   * Skips ahead to next index with space.
+   * @return Next word collected from start to next space, and returns
+   * next space index.
+   */
   private static NextWord getNextWord(String s, int start) {
     NextWord nextWord = new NextWord();
 
@@ -90,6 +108,7 @@ public class FaultyKeyboard {
     }
 
     nextWord.word = s.substring(start, i);
+    nextWord.index = i;
     return nextWord;
   }
 
@@ -99,7 +118,7 @@ public class FaultyKeyboard {
    * @return index of next space
    */
   private static int getNextSpace(String s, int start) {
-    int i = -1;
+    int i;
     for (i = start; i < s.length(); i++) {
       if (s.charAt(i) == ' ') {
         break;
@@ -119,12 +138,14 @@ public class FaultyKeyboard {
     }
   }
 
-  private static boolean isValidPrefix(String word, SuffixArray suffs) {
+  /**
+   * Checks if the prefix being built is headed in the right direction.
+   * If prefix is part of a valid word, return true else false.
+   */
+  private static boolean isValidPrefix(String prefix, Set<String> dict) {
     boolean isPrefix = false;
-    String suff;
-    for (int i = 0; i < suffs.length(); i++) {
-      suff = suffs.suffixes[i].toString();
-      if (suff.contains(word)) {
+    for (String validWord : dict) {
+      if (validWord.contains(prefix)) {
         isPrefix = true;
         break;
       }
