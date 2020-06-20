685a8ee16daf7887a9548f3fd7de15e21132788d
Anurag Patnaik <anurag.patnaik@autodesk.com>
2020-06-19 22:19:50 -0700

    [LeetCode] - NthNodeFromEndOfList - Partial Solution (187/208)

--- LeetCode/src/NthNodeFromEndOfList.java
+++ LeetCode/src/NthNodeFromEndOfList.java
@@ -2,19 +2,35 @@ import java.util.List;
 
 public class NthNodeFromEndOfList {
     public static void main(String[] args) {
+        // Test Case - 1
+//        ListNode node1 = new ListNode(1);
+//        ListNode node2 = new ListNode(2);
+//        ListNode node3 = new ListNode(3);
+//        ListNode node4 = new ListNode(4);
+//        ListNode node5 = new ListNode(5);
+//
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+//        node5.next = null;
+
+//        ListNode head = removeNthFromEnd(node1, 2);
+
+        // Test case - 2
+//        ListNode node1 = new ListNode(1);
+//        node1.next = null;
+//
+//        ListNode head = removeNthFromEnd(node1, 1);
+
+        // Test case - 3
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-
         node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = null;
+        node2.next = null;
 
         ListNode head = removeNthFromEnd(node1, 2);
+
         while (head != null) {
             System.out.print(head.val + ",");
             head = head.next;
@@ -25,6 +41,11 @@ public class NthNodeFromEndOfList {
         ListNode frontRunner = head;
         for (int i = 0; i < n; i++) {
             frontRunner = frontRunner.next;
+        }
+
+        // If list length = 1, and n = 1
+        if (frontRunner == null) {
+            return null;
         }
 
         // Stops right before nth node
