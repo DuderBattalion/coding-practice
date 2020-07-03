3151f64891b2389efa4387f36380a61731e6e8d4
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-02 22:44:06 -0700

    [LeetCode] SudokuSolver - Adding solution.

--- LeetCode/src/SudokuSolver.java
+++ LeetCode/src/SudokuSolver.java
@@ -5,7 +5,8 @@ import java.util.Set;
 
 public class SudokuSolver {
     public static void main(String[] args) {
-        char[][] board = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
+//        char[][] board = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
+        char[][] board = {{'.','.','9','7','4','8','.','.','.'},{'7','.','.','.','.','.','.','.','.'},{'.','2','.','1','.','9','.','.','.'},{'.','.','7','.','.','.','2','4','.'},{'.','6','4','.','1','.','5','9','.'},{'.','9','8','.','.','.','3','.','.'},{'.','.','.','8','.','3','.','2','.'},{'.','.','.','.','.','.','.','.','6'},{'.','.','.','2','7','5','9','.','.'}};
 
         printBoard(board);
         System.out.println("===================================");
@@ -78,11 +79,16 @@ public class SudokuSolver {
 
         boolean isValid = true;
         for (int j = 0; j < board.length; j++) {
-            if (processedNums.contains(num)) {
+//            if (processedNums.contains(num)) {
+//                isValid = false;
+//                break;
+//            } else {
+//                processedNums.add(board[i][j]);
+//            }
+
+            if (board[i][j] == num) {
                 isValid = false;
                 break;
-            } else {
-                processedNums.add(board[i][j]);
             }
         }
 
@@ -95,15 +101,24 @@ public class SudokuSolver {
                     String.format("Unexpected error [isColValid]: Col out of bounds. col: %d", j));
         }
 
-        Set<Character> processedNums = new HashSet<>();
+//        Set<Character> processedNums = new HashSet<>();
 
         boolean isValid = true;
         for (int i = 0; i < board.length; i++) {
-            if (processedNums.contains(num)) {
+            if (i == 1 && j == 5) {
+                int temp = 0;
+            }
+
+//            if (processedNums.contains(num)) {
+//                isValid = false;
+//                break;
+//            } else {
+//                processedNums.add(board[i][j]);
+//            }
+
+            if (board[i][j] == num) {
                 isValid = false;
                 break;
-            } else {
-                processedNums.add(board[i][j]);
             }
         }
 
