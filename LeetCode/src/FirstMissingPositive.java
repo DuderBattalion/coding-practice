b002acfb62ebcb0906a971d2ecb63b110ef02cf1
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-07 16:48:37 -0700

    [LeetCode] FirstMissingPositive - Partial solution - optimizing.
    Bugging out with some array index out of bounds issue.

--- LeetCode/src/FirstMissingPositive.java
+++ LeetCode/src/FirstMissingPositive.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.BitSet;
 
 public class FirstMissingPositive {
@@ -9,47 +10,84 @@ public class FirstMissingPositive {
 //        int[] nums = { 0, 0 };
 //        int[] nums = { -1, -2, -3 };
 //        int[] nums = { 1, 1000 };
-        int[] nums = { 1, 2, 3, 10, 2147483647, 9 };
+//        int[] nums = { 1, 2, 3, 10, 2147483647, 9 };
+//        int[] nums = { 0 };
+        int[] nums = { 1 };
 
         System.out.println(firstMissingPositive(nums));
     }
 
     public static int firstMissingPositive(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = 0;
+        // Observation: Missing positive has to be between 1 .. N (N being length of nums)
+        // Skip all negative numbers
+        // All numbers more than N can also be made negative so they are skipped
+        // Process remaining numbers, for each num[i] - make that index negative
+        // The first positive index left is missing number
 
-        for (int num: nums) {
-            if (num < 0) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums.length || nums[i] < 1) {
+                nums[i] = -1;
                 continue;
             }
 
-            if (num < min) {
-                min = num;
+            // num between 1 .. N
+            nums[nums[i] - 1] = -2; // Zero index's - so (index - 1)
+        }
+
+        int missingNum = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == -2) {
+                if (i + i < nums.length && nums[i+1] != -2) {
+                    missingNum = (i + 1) + 1; // Adjust for zero index
+                    break;
+                } else if (i + 1 == nums.length){
+                    missingNum = i + 1;
+                    break;
+                }
             }
-
-            if (num > max) {
-                max = num;
-            }
         }
 
-        if (min > 1) {
-            return 1;
-        }
-
-        // min == 0 or 1 from here
-        if (max == 1) {
-            return 2;
-        }
-
-        BitSet bitSet = new BitSet(max);
-        for (int num: nums) {
-            if (num <= 0) {
-                continue;
-            }
-
-            bitSet.set(num-1); // 0 index'd
-        }
-
-        return bitSet.nextClearBit(1) + 1; // adjust for zero index
+        return missingNum;
     }
+
+//    public static int firstMissingPositive(int[] nums) {
+//        int min = Integer.MAX_VALUE;
+//        int max = 0;
+//
+//        for (int num: nums) {
+//            if (num <= 0) {
+//                continue;
+//            }
+//
+//            if (num < min) {
+//                min = num;
+//            }
+//
+//            if (num > max) {
+//                max = num;
+//            }
+//        }
+//
+//        if (min > 1) {
+//            return 1;
+//        }
+//
+//        // min == 0 or 1 from here
+//        if (max == 1) {
+//            return 2;
+//        }
+//
+//        BitSet bitSet = new BitSet(max);
+//        for (int num: nums) {
+//            if (num <= 0) {
+//                continue;
+//            }
+//
+//            bitSet.set(num-1); // 0 index'd
+//        }
+//
+//        return bitSet.nextClearBit(1) + 1; // adjust for zero index
+//    }
 }
