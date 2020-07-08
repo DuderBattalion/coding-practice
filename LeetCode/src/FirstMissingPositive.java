f65c9789842a7bb3eb716ea73fe7a179fe258627
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-08 16:03:55 -0700

    [LeetCode] FirstMissingPositive - Partial solution (114/165)

--- LeetCode/src/FirstMissingPositive.java
+++ LeetCode/src/FirstMissingPositive.java
@@ -6,13 +6,14 @@ public class FirstMissingPositive {
 //        int[] nums = { 7, 8, 9, 11, 12 };
 //        int[] nums = { 3, 4, -1, 1 };
 //        int[] nums = { 1, 2, 0 };
-//        int[] nums = { 1, 1 };
+        int[] nums = { 1, 1 };
 //        int[] nums = { 0, 0 };
 //        int[] nums = { -1, -2, -3 };
-        int[] nums = { 1, 1000 };
+//        int[] nums = { 1, 1000 };
 //        int[] nums = { 1, 2, 3, 10, 2147483647, 9 };
 //        int[] nums = { 0 };
 //        int[] nums = { 1 };
+//        int[] nums = { 2, 2, 2, 2, 2 };
 
         System.out.println(firstMissingPositive(nums));
     }
@@ -24,28 +25,64 @@ public class FirstMissingPositive {
         // Process remaining numbers, for each num[i] - make that index negative
         // The first positive index left is missing number
 
+        final int NUM_NEGATIVE = -2;
+        final int NUM_PROCESSED = 0;
+        final int NUM_IGNORED = -1;
+
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
+            // Out of range values are ignored
             if (nums[i] > nums.length || nums[i] < 1) {
-                nums[i] = -1;
-                continue;
+                nums[i] = NUM_IGNORED;
             }
-
-            // num between 1 .. N
-            nums[nums[i] - 1] = -2; // Zero index's - so (index - 1)
         }
 
-        int missingNum = 1;
+        // Numbers guaranteed to be between 0 .. N, with 0 signifying processed index
+        // At the end of this loop, all numbers are either:
+        // a) Ignored (-1)
+        // b) nums[i] index has been tagged with (0) to mark presence
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == -2) {
-                if (i + 1 < nums.length && nums[i+1] != -2) {
-                    missingNum = (i + 1) + 1; // Adjust for zero index
-                    break;
-                } else if (i + 1 == nums.length){
-                    missingNum = (i + 1) + 1; // Adjust for zero index
-                    break;
+            if (nums[i] < 1) {
+                continue; // Skip out of range numbers
+            }
+
+            int targetIndex = nums[i] - 1;
+
+            // num between 1 .. N
+            nums[targetIndex] = NUM_PROCESSED; // Zero index's - so (index - 1)
+
+            if (i != targetIndex) { // Zero index'd
+                nums[i] = NUM_IGNORED; // Set processed numbers to be ignored in next pass
+            }
+        }
+
+        int missingNum = -1;
+        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == -2) {
+//                if (i + 1 < nums.length && nums[i+1] != -2) {
+//                    missingNum = (i + 1) + 1; // Adjust for zero index
+//                    break;
+//                } else if (i + 1 == nums.length){
+//                    missingNum = (i + 1) + 1; // Adjust for zero index
+//                    break;
+//                }
+//            }
+
+            // Last cell
+            if (i == (nums.length - 1)) {
+                if (nums[i] == NUM_PROCESSED) {
+                    missingNum = (i + 1) + 1; // All indexes are valid, +1 for zero index adjust
+                } else {
+                    missingNum = 1; // All indexes are ignored
                 }
+
+                break;
+            }
+
+            if (nums[i] == NUM_PROCESSED && nums[i+1] == NUM_IGNORED) {
+                missingNum = (i + 1) + 1; // Zero index adjust
+                break;
             }
         }
 
