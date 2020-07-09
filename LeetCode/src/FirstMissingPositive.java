42908ac801a89a14788e36f2c030a7f940345eb7
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-09 14:49:10 -0700

    [LeetCode] FirstMissingPositive - Code cleanup.

--- LeetCode/src/FirstMissingPositive.java
+++ LeetCode/src/FirstMissingPositive.java
@@ -51,44 +51,18 @@ public class FirstMissingPositive {
                 continue; // Skip out of range numbers
             }
 
-            int targetIndex = nums[i] - 1;
+            int targetIndex = nums[i] - 1; // Zero index'd (hence index - 1)
 
             // num between 1 .. N
-            nums[targetIndex] = NUM_PROCESSED; // Zero index's - so (index - 1)
+            nums[targetIndex] = NUM_PROCESSED;
 
-            if (i != targetIndex) { // Zero index'd
+            if (i != targetIndex) {
                 nums[i] = NUM_IGNORED; // Set processed numbers to be ignored in next pass
             }
         }
 
         int missingNum = -1;
         for (int i = 0; i < nums.length; i++) {
-//            if (nums[i] == -2) {
-//                if (i + 1 < nums.length && nums[i+1] != -2) {
-//                    missingNum = (i + 1) + 1; // Adjust for zero index
-//                    break;
-//                } else if (i + 1 == nums.length){
-//                    missingNum = (i + 1) + 1; // Adjust for zero index
-//                    break;
-//                }
-//            }
-
-//            // Last cell
-//            if (i == (nums.length - 1)) {
-//                if (nums[i] == NUM_PROCESSED) {
-//                    missingNum = (i + 1) + 1; // All indexes are valid, +1 for zero index adjust
-//                } else {
-//                    missingNum = 1; // All indexes are ignored
-//                }
-//
-//                break;
-//            }
-//
-//            if (nums[i] == NUM_PROCESSED && nums[i+1] == NUM_IGNORED) {
-//                missingNum = (i + 1) + 1; // Zero index adjust
-//                break;
-//            }
-
             if (nums[i] != NUM_PROCESSED) {
                 missingNum = i + 1; // Zero index adjust
                 break;
@@ -102,43 +76,4 @@ public class FirstMissingPositive {
 
         return missingNum;
     }
-
-//    public static int firstMissingPositive(int[] nums) {
-//        int min = Integer.MAX_VALUE;
-//        int max = 0;
-//
-//        for (int num: nums) {
-//            if (num <= 0) {
-//                continue;
-//            }
-//
-//            if (num < min) {
-//                min = num;
-//            }
-//
-//            if (num > max) {
-//                max = num;
-//            }
-//        }
-//
-//        if (min > 1) {
-//            return 1;
-//        }
-//
-//        // min == 0 or 1 from here
-//        if (max == 1) {
-//            return 2;
-//        }
-//
-//        BitSet bitSet = new BitSet(max);
-//        for (int num: nums) {
-//            if (num <= 0) {
-//                continue;
-//            }
-//
-//            bitSet.set(num-1); // 0 index'd
-//        }
-//
-//        return bitSet.nextClearBit(1) + 1; // adjust for zero index
-//    }
 }
