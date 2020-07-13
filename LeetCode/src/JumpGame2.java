24356449d1bc76d5e669d7100622d8c6b209b9c2
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-13 13:26:02 -0700

    [LeetCode] Jumpgate 2 - WIP commit.
    Optimizing bfs.

--- LeetCode/src/JumpGame2.java
+++ LeetCode/src/JumpGame2.java
@@ -3,8 +3,9 @@ import java.util.Queue;
 
 public class JumpGame2 {
     public static void main(String[] args) {
-        int[] nums = { 2, 3, 1, 1, 4 };
+//        int[] nums = { 2, 3, 1, 1, 4 };
 //        int[] nums = { 0 };
+        int[] nums = { 1, 2, 1, 1, 1 };
 
         System.out.println(jump(nums));
     }
@@ -17,32 +18,62 @@ public class JumpGame2 {
         Queue<Integer> frontier = new LinkedList<>();
         frontier.offer(0);
 
-        // Modified BFS
-        int jumpCount = 0;
         boolean isLastIndex = false;
         while (!frontier.isEmpty()) {
             int index = frontier.poll();
+
+            // Check neighbors
             for (int jump = 1; jump <= nums[index]; jump++) {
                 int nextIndex = index + jump;
                 if (nextIndex == (nums.length - 1)) {
                     isLastIndex = true;
-                    jumpCount++;
                     break;
-                } else if (nextIndex >= nums.length) {
-                    continue; // Out of bounds
                 }
 
-                frontier.offer(nextIndex);
-            }
 
-            if (isLastIndex) {
-                break;
             }
+        }
+    }
 
-            jumpCount++;
+    /**
+     * Searches neighbors if they have last index.
+     * If not, then return the next level of neighbors.
+     */
+    private static boolean calcMinJumps(int[] nums, int index, int jumps, Queue<Integer> neighbors) {
+        neighbors = calcNeighbors(nums, index);
+        if (neighbors.isEmpty()) {
+            return false;
         }
 
-        // If last index reached, return jumpCount, or return 0
-        return isLastIndex ? jumpCount : 0;
+        if (isContainLastIndex(neighbors)) {
+            return true;
+        }
+
+
+    }
+
+    private static Queue<Integer> calcNeighbors(int[] nums, int index) {
+        Queue<Integer> neighbors = new LinkedList<>();
+
+        if (nums.length == 0 || index >= nums.length) {
+            return neighbors;
+        }
+
+        int maxJumps = nums[index];
+        for (int i = 1; i <= maxJumps; i++) {
+            int nextIndex = index + i;
+            neighbors.add(nextIndex);
+
+            if (nextIndex == (nums.length - 1)) {
+                // Last index found - stop processing
+                break;
+            }
+        }
+
+        return neighbors;
+    }
+
+    private static boolean isContainLastIndex(Queue<Integer> neighbors, int lastIndex) {
+        neighbors.contains(lastIndex);
     }
 }
