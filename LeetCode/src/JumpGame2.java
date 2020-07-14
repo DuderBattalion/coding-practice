79ff8672d8e1df982936caa323b3f4a3459bb46a
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-13 21:58:20 -0700

    [LeetCode] JumpGate2 - Adding solution.

--- LeetCode/src/JumpGame2.java
+++ LeetCode/src/JumpGame2.java
@@ -7,14 +7,18 @@ public class JumpGame2 {
     public static void main(String[] args) {
 //        int[] nums = { 2, 3, 1, 1, 4 };
 //        int[] nums = { 0 };
-//        int[] nums = { 1, 2, 1, 1, 1 };
+        int[] nums = { 1, 2, 1, 1, 1 };
 //        int[] nums = { 1 };
-        int[] nums = { 8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5 };
+//        int[] nums = { 8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5 };
 
         System.out.println(jump2(nums));
     }
 
     public static int jump2(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return 0;
+        }
+
         int jumps = 0;
 
         Queue<Integer> neighborIndices = new LinkedList<>();
@@ -33,13 +37,20 @@ public class JumpGame2 {
                 }
 
                 int neighborIndex = neighborIndices.poll();
+
+                // If last index lies in jump range, break
+                if (nums[neighborIndex] >= (nums.length - neighborIndex)) {
+                    isLastIndexFound = true;
+                    break;
+                }
+
                 if (visitedNodes.contains(neighborIndex)) {
                     continue;
                 }
 
                 visitedNodes.add(neighborIndex);
 
-                for (int j = 1; j <= nums[neighborIndex]; j++) {
+                for (int j = nums[neighborIndex]; j >= 1; j--) {
                     int nextIndex = neighborIndex + j;
                     if (nextIndex >= nums.length) {
                         break; // out of bounds
