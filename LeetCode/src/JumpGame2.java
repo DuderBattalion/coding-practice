45cb58ca56a33791d3ed35b723460f3af51064ec
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-13 17:38:54 -0700

    [LeetCode] JumpGate2 - WIP - optimizing solution.

--- LeetCode/src/JumpGame2.java
+++ LeetCode/src/JumpGame2.java
@@ -6,12 +6,70 @@ import java.util.Set;
 public class JumpGame2 {
     public static void main(String[] args) {
 //        int[] nums = { 2, 3, 1, 1, 4 };
-//        int[] nums = { 0 };
+        int[] nums = { 0 };
 //        int[] nums = { 1, 2, 1, 1, 1 };
 //        int[] nums = { 1 };
-        int[] nums = { 8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5 };
+//        int[] nums = { 8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5 };
 
-        System.out.println(jump(nums));
+        System.out.println(jump2(nums));
+    }
+
+    public static int jump2(int[] nums) {
+        int jumps = 0;
+
+        Queue<Integer> neighborIndices = new LinkedList<>();
+        Set<Integer> visitedNodes = new HashSet<>();
+
+        // Seed with first index
+        neighborIndices.add(0);
+
+        boolean isLastIndexFound = false;
+        while (!neighborIndices.isEmpty()) {
+            int levelSize = neighborIndices.size();
+            for (int i = 0; i < levelSize; i++) {
+                // Should never happen, but IDE complains if omitted
+                if (neighborIndices.isEmpty()) {
+                    break;
+                }
+
+                int neighborIndex = neighborIndices.poll();
+                if (visitedNodes.contains(neighborIndex)) {
+                    continue;
+                }
+
+                visitedNodes.add(neighborIndex);
+
+                for (int j = 1; j <= nums[neighborIndex]; j++) {
+                    int nextIndex = neighborIndex + j;
+                    if (nextIndex >= nums.length) {
+                        break; // out of bounds
+                    }
+
+                    // Last index
+                    if (nextIndex == (nums.length - 1)) {
+                        isLastIndexFound = true;
+                        break;
+                    }
+
+                    if (visitedNodes.contains(nextIndex)) {
+                        continue;
+                    }
+
+                    neighborIndices.add(nextIndex);
+                }
+
+                if (isLastIndexFound) {
+                    break;
+                }
+            }
+
+            jumps++;
+            if (isLastIndexFound) {
+                break;
+            }
+        }
+
+        return jumps;
     }
 
     /**
