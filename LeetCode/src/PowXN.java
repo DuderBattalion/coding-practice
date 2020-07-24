01d4e564a2f1c6390342eb9b8f1f86448a808e6d
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-23 21:55:52 -0700

    [LeetCode] PowXN - Partial Solution (303/304)

--- LeetCode/src/PowXN.java
+++ LeetCode/src/PowXN.java
@@ -5,9 +5,24 @@ public class PowXN {
         System.out.println(myPow(2, -2));
         System.out.println(myPow(-2, 4));
         System.out.println(myPow(99, Integer.MAX_VALUE));
+        System.out.println(myPow(99, Integer.MIN_VALUE));
+        System.out.println(myPow(1, Integer.MIN_VALUE));
+        System.out.println(myPow(-1, Integer.MIN_VALUE));
     }
 
     public static double myPow(double x, int n) {
+        if (x == 1) {
+            return 1;
+        }
+
+        if (x == -1) {
+            return (n % 2) == 0 ? 1 : 0;
+        }
+
+        if (n == Integer.MIN_VALUE) {
+            return 0;
+        }
+
         if (n < 0) {
             return (1 / myPowRecurse(x, n * -1));
         } else {
