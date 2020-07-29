383e8359df294d7c0a02e945009410a077bc1faf
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-29 11:21:55 -0700

    [LeetCode] JumpGame - optimized solution.

--- LeetCode/src/JumpGame.java
+++ LeetCode/src/JumpGame.java
@@ -3,6 +3,9 @@ import java.util.Queue;
 import java.util.Set;
 
 public class JumpGame {
+    public static final int GOOD_INDEX_TOKEN = -1;
+    public static final int BAD_INDEX_TOKEN = -2;
+
     public static void main(String[] args) {
 //        int[] nums = { 2, 3, 1, 1, 4 };
 //        int[] nums = { 3, 2, 1, 0, 4 };
@@ -13,42 +16,50 @@ public class JumpGame {
         System.out.println(canJump(nums));
     }
 
+    /**
+     * Algorithm
+     * Good Index: An index from which there is a path to the last index
+     * Bad Index: A dead end index which cannot access the last index
+     *
+     * Start from the last index and mark it as a good index.
+     * Now start moving backwards toward index[0] and mark each index with a good or bad token, depending on
+     * if there is a path to a good index within the range of the current index.
+     *
+     * Once the pass is complete, the first index will have a good or bad index value. Return this value.
+     */
     public static boolean canJump(int[] nums) {
-        if (nums.length == 0) {
+        if (nums.length <= 1) {
             return true;
         }
 
-        return canJumpDfs(nums, 0, new HashSet<Integer>());
+        int i = nums.length - 1;
+        nums[i] = GOOD_INDEX_TOKEN;
+        i--;
+
+        boolean canJumpStatus = true;
+        while (i >= 0) {
+            canJumpStatus = isNeighborGoodIndex(i, nums);
+            if (canJumpStatus) {
+                nums[i] = GOOD_INDEX_TOKEN;
+            } else {
+                nums[i] = BAD_INDEX_TOKEN;
+            }
+
+            i--;
+        }
+
+        return nums[0] == GOOD_INDEX_TOKEN;
     }
 
-    private static boolean canJumpDfs(int[] nums, int i, Set<Integer> indexCache) {
-        if (indexCache.contains(i)) {
-            return false;
-        }
-
-        if (i < 0 || i >= nums.length) {
-            indexCache.add(i);
-            return false;
-        }
-
-        if (i == (nums.length - 1)) {
-            return true;
-        }
-
-        if (nums[i] == 0) {
-            indexCache.add(i);
-            return false;
-        }
-
-        boolean isLastIndex = false;
-        for (int j = 1; j <= nums[i]; j++) {
-            if (canJumpDfs(nums, i + j, indexCache)) {
-                isLastIndex = true;
+    private static boolean isNeighborGoodIndex(int i, int[] nums) {
+        boolean isGoodIndex = false;
+        for (int j = nums[i]; j >= 0; j--) {
+            if ((i + j) >= (nums.length - 1) || nums[i + j] == GOOD_INDEX_TOKEN) {
+                isGoodIndex = true;
                 break;
             }
         }
 
-        indexCache.add(i);
-        return isLastIndex;
+        return isGoodIndex;
     }
 }
