9119c2d63c07b546cf3134b87c589b58849c1ebf
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-28 22:48:13 -0700

    [LeetCode] JumpGame - Base case working (1/2)

---
+++ LeetCode/src/JumpGame.java
@@ -0,0 +1,49 @@
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public class JumpGame {
+    public static void main(String[] args) {
+//        int[] nums = { 2, 3, 1, 1, 4 };
+        int[] nums = { 3, 2, 1, 0, 4 };
+
+        System.out.println(canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+        if (nums.length == 0) {
+            return true;
+        }
+
+        return canJumpDfs(nums, 0, new HashSet<Integer>());
+    }
+
+    private static boolean canJumpDfs(int[] nums, int i, Set<Integer> indexCache) {
+        if (indexCache.contains(i)) {
+            return indexCache.remove(i);
+        }
+
+        if (i < 0 || i >= nums.length) {
+            return false;
+        }
+
+        if (i == (nums.length - 1)) {
+            return true;
+        }
+
+        if (nums[i] == 0) {
+            return false;
+        }
+
+        boolean isLastIndex = false;
+        for (int j = 1; j <= nums[i]; j++) {
+            if (canJumpDfs(nums, i + j, indexCache)) {
+                isLastIndex = true;
+                break;
+            }
+        }
+
+        indexCache.add(i);
+        return isLastIndex;
+    }
+}
