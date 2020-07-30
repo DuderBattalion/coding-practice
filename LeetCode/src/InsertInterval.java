29dc9914e5c70f9cc3af45595d55356f4fc35c16
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-07-30 11:18:05 -0700

    [LeetCode] InsertIntervals - Adding solution.

--- LeetCode/src/InsertInterval.java
+++ LeetCode/src/InsertInterval.java
@@ -1,13 +1,39 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class InsertInterval {
     public static void main(String[] args) {
-        int[][] intervals = {
-                {1, 3},
-                {6, 9}
-        };
+//        int[][] intervals = {
+//                {1, 3},
+//                {6, 9}
+//        };
+//
+//        int[] newInterval = { 2, 5 };
 
-        int[] newInterval = { 2, 5 };
+//        int[][] intervals = {
+//                {1, 2},
+//                {3, 5},
+//                {6, 7},
+//                {8, 10},
+//                {12, 16},
+//                {18, 20}
+//        };
+//
+//        int[] newInterval = {4, 8};
+
+//        int[][] intervals = {{2, 3}};
+//        int[] newInterval = {4, 8};
+
+//        int[][] intervals = {{4, 8}};
+//        int[] newInterval = {2, 3};
+
+//        int[][] intervals = {{4, 8}};
+//        int[] newInterval = {2, 5};
+
+        int[][] intervals = {{3, 5}, {12, 15}};
+        int[] newInterval = {6, 6};
 
         int[][] output = insert(intervals, newInterval);
         for (int[] interval: output) {
@@ -26,24 +52,50 @@ public class InsertInterval {
 
         Interval newIntervalObj = new Interval(newInterval[0], newInterval[1]);
         LinkedList<Interval> intervalList = createInputFormat(intervals);
+        Deque<Interval> output = new ArrayDeque<>();
+
+        // Special Case: Insert at head
+        boolean isMergedAtHead = false;
+        if (newIntervalObj.end < intervalList.get(0).start) {
+            output.push(newIntervalObj);
+            isMergedAtHead = true;
+        }
 
         boolean isMergePositionFound = false;
+        boolean isInserted = false;
         for (int i = 0; i < intervalList.size(); i++) {
             Interval interval = intervalList.get(i);
             if (interval.isOverlap(newIntervalObj)) {
                 Interval mergedInterval = mergeInterval(interval, newIntervalObj);
-                intervalList.set(i, mergedInterval);
+//                intervalList.set(i, mergedInterval);
+
+                if (isMergePositionFound) {
+                    // Continuation of merge, pop last output node
+                    // and push in new merged interval
+                    output.pop();
+                }
+
+                output.push(mergedInterval);
 
                 newIntervalObj = mergedInterval;
                 isMergePositionFound = true;
-            } else if (isMergePositionFound) {
-                // If merge position was previously found, and we run out of overlap
-                // then we are done. Break out of loop.
-                break;
+            } else if (!isInserted && (i + 1) < intervalList.size()
+                    && canInsert(interval, intervalList.get(i + 1), newIntervalObj)) {
+                output.push(interval);
+                output.push(newIntervalObj);
+
+                isInserted = true;
+            } else {
+                output.push(interval);
             }
         }
 
-        return createOutputFormat(intervalList);
+        // Special case: Add to tail
+        if (!isMergePositionFound && !isMergedAtHead && !isInserted) {
+            output.push(newIntervalObj);
+        }
+
+        return createOutputFormat(output);
     }
 
     private static class Interval implements Comparable<Interval> {
@@ -91,13 +143,24 @@ public class InsertInterval {
                 Math.max(interval1.end, interval2.end));
     }
 
-    private static int[][] createOutputFormat(LinkedList<Interval> intervalList) {
-        int[][] output = new int[intervalList.size()][2];
+    private static boolean canInsert(Interval interval1, Interval interval2, Interval interval) {
+        if (interval1 == null || interval2 == null || interval == null) {
+            return false;
+        }
 
-        for (int i = 0; i < intervalList.size(); i++) {
-            Interval interval = intervalList.get(i);
-            output[i][0] = interval.start;;
-            output[i][1] = interval.end;;
+        return ((interval.start > interval1.start) && (interval.end < interval2.start));
+    }
+
+    private static int[][] createOutputFormat(Deque<Interval> outputStack) {
+        int[][] output = new int[outputStack.size()][2];
+
+        int i = outputStack.size() - 1;
+        while(!outputStack.isEmpty()) {
+            Interval interval = outputStack.pop();
+            output[i][0] = interval.start;
+            output[i][1] = interval.end;
+
+            i--;
         }
 
         return output;
