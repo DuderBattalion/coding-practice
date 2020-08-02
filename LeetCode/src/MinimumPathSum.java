ea789efe7e9d0cf4f92b4d1de533c0e584894fdd
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-08-01 22:43:46 -0700

    [LeetCode] MinimumPathSum - Partial solution (60/61)

--- LeetCode/src/MinimumPathSum.java
+++ LeetCode/src/MinimumPathSum.java
@@ -1,9 +1,18 @@
 public class MinimumPathSum {
     public static void main(String[] args) {
+//        int[][] grid = {
+//                {1,3,1},
+//                {1,5,1},
+//                {4,2,1}
+//        };
+
+//        int[][] grid = {};
+
+//        int[][] grid = { {1, 2, 3} };
         int[][] grid = {
-                {1,3,1},
-                {1,5,1},
-                {4,2,1}
+                {1},
+                {2},
+                {3}
         };
 
         System.out.println(minPathSum(grid));
