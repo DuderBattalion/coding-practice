ed186b6b68fe6f86e5e1e5921c64912e69b39daf
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-08-12 17:25:24 -0700

    [LeetCode] RemoveDuplicatesFromSortedArray2 - Base case working (1/2).

---
+++ LeetCode/src/RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,46 @@
+public class RemoveDuplicatesFromSortedArray2 {
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
+
+        int count = removeDuplicates(nums);
+        for (int i = 0; i < count; i++) {
+            System.out.print(nums[i] + ",");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0, insertIndex = 1;
+        while (i < nums.length) {
+            if (i + 1 == nums.length) {
+                break;
+            }
+
+            if (nums[i] == nums[i + 1]) {
+                i++;
+                insertIndex++;
+            }
+
+            int nextI = findNextNum(i, nums);
+            if (nextI > 0) {
+                nums[insertIndex] = nums[nextI];
+                i = nextI;
+                insertIndex++;
+            }
+        }
+
+        return insertIndex;
+    }
+
+    private static int findNextNum(int index, int[] nums) {
+        int num = nums[index];
+        int nextIndex = -1;
+        for (int i = index + 1; i < nums.length; i++) {
+            if (nums[i] != num) {
+                nextIndex = i;
+                break;
+            }
+        }
+
+        return nextIndex;
+    }
+}
