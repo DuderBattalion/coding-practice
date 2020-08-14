596f0de67bf6aa0c7b09fb12de02e44ccb3ee482
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-08-14 14:52:34 -0700

    [LeetCode] RemoveDuplicateFromSortedList2 - Simplifying code.

--- LeetCode/src/RemoveDuplicatesFromSortedList2.java
+++ LeetCode/src/RemoveDuplicatesFromSortedList2.java
@@ -4,34 +4,34 @@ import java.util.List;
 
 public class RemoveDuplicatesFromSortedList2 {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(1);
-        ListNode list2 = new ListNode(2);
-        ListNode list3 = new ListNode(3);
-        ListNode list4 = new ListNode(3);
-        ListNode list5 = new ListNode(4);
-        ListNode list6 = new ListNode(4);
-        ListNode list7 = new ListNode(5);
-
-        list1.next = list2;
-        list2.next = list3;
-        list3.next = list4;
-        list4.next = list5;
-        list5.next = list6;
-        list6.next = list7;
-        list7.next = null;
-
-        // Test case 2
 //        ListNode list1 = new ListNode(1);
-//        ListNode list2 = new ListNode(1);
-//        ListNode list3 = new ListNode(1);
-//        ListNode list4 = new ListNode(2);
-//        ListNode list5 = new ListNode(3);
+//        ListNode list2 = new ListNode(2);
+//        ListNode list3 = new ListNode(3);
+//        ListNode list4 = new ListNode(3);
+//        ListNode list5 = new ListNode(4);
+//        ListNode list6 = new ListNode(4);
+//        ListNode list7 = new ListNode(5);
 //
 //        list1.next = list2;
 //        list2.next = list3;
 //        list3.next = list4;
 //        list4.next = list5;
-//        list5.next = null;
+//        list5.next = list6;
+//        list6.next = list7;
+//        list7.next = null;
+
+        // Test case 2
+        ListNode list1 = new ListNode(1);
+        ListNode list2 = new ListNode(1);
+        ListNode list3 = new ListNode(1);
+        ListNode list4 = new ListNode(2);
+        ListNode list5 = new ListNode(3);
+
+        list1.next = list2;
+        list2.next = list3;
+        list3.next = list4;
+        list4.next = list5;
+        list5.next = null;
 
         ListNode node = deleteDuplicates(list1);
         while (node != null) {
@@ -46,25 +46,52 @@ public class RemoveDuplicatesFromSortedList2 {
 
         ListNode node = dummy;
         while (node != null) {
-            ListNode nextNode = node.next;
-            boolean isNextNodeDuplicate = isNodeDuplicate(nextNode);
+//            ListNode nextNode = node.next;
+//            boolean isNextNodeDuplicate = isNodeDuplicate(nextNode);
+//
+//            // Skip ALL adjacent duplicate nodes
+//            while (isNextNodeDuplicate) {
+//                ListNode nextNextNode = findNextNode(nextNode);
+//                isNextNodeDuplicate = isNodeDuplicate(nextNextNode);
+//                if (!isNextNodeDuplicate) {
+//                    node.next = nextNextNode;
+//                    break;
+//                } else {
+//                    nextNode = nextNextNode;
+//                }
+//            }
+//
+//            node = node.next;
 
-            // Skip ALL adjacent duplicate nodes
-            while (isNextNodeDuplicate) {
-                ListNode nextNextNode = findNextNode(nextNode);
-                isNextNodeDuplicate = isNodeDuplicate(nextNextNode);
-                if (!isNextNodeDuplicate) {
-                    node.next = nextNextNode;
-                    break;
-                } else {
-                    nextNode = nextNextNode;
-                }
-            }
-
+            node.next = skipAllDuplicateNodes(node.next);
             node = node.next;
         }
 
         return dummy.next;
+    }
+
+    /**
+     * Skips all duplicate nodes from origin, and returns the next node
+     * that is not duplicate. Returns null if no such node found.
+     */
+    private static ListNode skipAllDuplicateNodes(ListNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        // Skip ALL adjacent duplicate nodes
+        while (isNodeDuplicate(node)) {
+            node = findNextNode(node);
+//            boolean isNextNodeDuplicate = isNodeDuplicate(nextNode);
+//            if (!isNextNodeDuplicate) {
+//                node.next = nextNextNode;
+//                break;
+//            } else {
+//                nextNode = nextNextNode;
+//            }
+        }
+
+        return node;
     }
 
     private static boolean isNodeDuplicate(ListNode node) {
