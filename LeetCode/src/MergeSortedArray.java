edf8adadb067dd07b5f2b422018cfbce7a793cab
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-08-18 12:28:55 -0700

    [LeetCode] MergeSortedArray - Adding solution.

--- LeetCode/src/MergeSortedArray.java
+++ LeetCode/src/MergeSortedArray.java
@@ -1,9 +1,12 @@
 public class MergeSortedArray {
     public static void main(String[] args) {
-        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+//        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+//        int[] nums2 = { 2, 5, 6 };
+
+        int[] nums1 = { 1, 2, 3 };
         int[] nums2 = { 2, 5, 6 };
 
-        merge(nums1, 3, nums2, 3);
+        merge(nums1, 3, nums2, 0);
         for (int num: nums1){
             System.out.print(num + ", ");
         }
