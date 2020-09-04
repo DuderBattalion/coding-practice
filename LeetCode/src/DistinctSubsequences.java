2ee036a05e813723dece47a6076bcdfd1e48b7dd
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-04 13:20:45 -0700

    [LeetCode] DistinctSubsequences - Base case working.

--- LeetCode/src/DistinctSubsequences.java
+++ LeetCode/src/DistinctSubsequences.java
@@ -1,7 +1,11 @@
 public class DistinctSubsequences {
     public static void main(String[] args) {
-//        System.out.println(numDistinct("rabbbit", "rabbit"));
+        System.out.println(numDistinct("rabbbit", "rabbit"));
         System.out.println(numDistinct("babgbag", "bag"));
+
+        System.out.println(numDistinct("", "bag"));
+        System.out.println(numDistinct("abcde", ""));
+        System.out.println(numDistinct("", ""));
     }
 
     public static int numDistinct(String s, String t) {
@@ -13,10 +17,6 @@ public class DistinctSubsequences {
     }
 
     private static int calculateNumDistinct(String s, String t, int i, int count, String subSequence) {
-        if (subSequence.equals(t)) {
-            count++;
-        }
-
         if (i >= s.length()) {
             return count;
         }
@@ -24,13 +24,16 @@ public class DistinctSubsequences {
         // Cases:
         // 1) Skip s.charAt(i)
         // 2) Include s.charAt(i)
-        char token = s.charAt(i);
 
         // Case 1: Skip s.charAt(i)
         count = calculateNumDistinct(s, t, i + 1, count, subSequence);
 
         // Case 2: Include s.charAt(i)
-        count = calculateNumDistinct(s, t, i + 1, count, subSequence + token);
+        subSequence += s.charAt(i);
+        if (subSequence.equals(t)) {
+            count++;
+        }
+        count = calculateNumDistinct(s, t, i + 1, count, subSequence);
 
         return count;
     }
