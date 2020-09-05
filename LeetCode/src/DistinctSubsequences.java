c204dfab88c9a26055897e5625edcb64e272760f
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-04 17:42:12 -0700

    [LeetCode] DistinctSubsequences - Adding solution.

--- LeetCode/src/DistinctSubsequences.java
+++ LeetCode/src/DistinctSubsequences.java
@@ -2,19 +2,27 @@ public class DistinctSubsequences {
     public static void main(String[] args) {
         System.out.println(numDistinct("rabbbit", "rabbit"));
         System.out.println(numDistinct("babgbag", "bag"));
-//        System.out.println(numDistinct("bccbcdcabadabddbccaddcbabbaaacdba", "bccbbdc"));
+        System.out.println(numDistinct("bccbcdcabadabddbccaddcbabbaaacdba", "bccbbdc"));
 
-//        System.out.println(numDistinct("", "bag"));
-//        System.out.println(numDistinct("abcde", ""));
-//        System.out.println(numDistinct("", ""));
+        System.out.println(numDistinct("", "bag"));
+        System.out.println(numDistinct("abcde", ""));
+        System.out.println(numDistinct("", ""));
     }
 
+    /**
+     * Algorithm: DP solution
+     * Rows are target subsequence characters, columns are source string
+     * Recurrence formula:
+     * dp[i][j] = All distinct subsequences prior to current target subsequence character (dp[i-1][j-1])
+     *      + all subsequences calculated so far for this particular target subsequence character.
+     * Disclaimer - some intuition taken from discussion:
+     * https://leetcode.com/problems/distinct-subsequences/discuss/37327/Easy-to-understand-DP-in-Java
+     */
     public static int numDistinct(String s, String t) {
         if (s.length() < t.length()) {
             return 0;
         }
 
-//        return calculateNumDistinct(s, t, 0, 0, "");
         int[][] dp = new int[t.length() + 1][s.length() + 1];
         initDp(dp, s, t);
 
@@ -28,18 +36,7 @@ public class DistinctSubsequences {
             }
         }
 
-//        debugDp(dp, t.length(), s.length());
         return dp[t.length()][s.length()];
-    }
-
-    private static void debugDp(int[][] dp, int rows, int cols) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-
-            System.out.println();
-        }
     }
 
     private static void initDp(int[][] dp, String s, String t) {
@@ -47,27 +44,4 @@ public class DistinctSubsequences {
             dp[0][j] = 1;
         }
     }
-
-//    // TODO - convert to a dp solution to increase performance
-//    private static int calculateNumDistinct(String s, String t, int i, int count, String subSequence) {
-//        if (i >= s.length()) {
-//            return count;
-//        }
-//
-//        // Cases:
-//        // 1) Skip s.charAt(i)
-//        // 2) Include s.charAt(i)
-//
-//        // Case 1: Skip s.charAt(i)
-//        count = calculateNumDistinct(s, t, i + 1, count, subSequence);
-//
-//        // Case 2: Include s.charAt(i)
-//        subSequence += s.charAt(i);
-//        if (subSequence.equals(t)) {
-//            count++;
-//        }
-//        count = calculateNumDistinct(s, t, i + 1, count, subSequence);
-//
-//        return count;
-//    }
 }
