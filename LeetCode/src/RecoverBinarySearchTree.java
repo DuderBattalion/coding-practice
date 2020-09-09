ef82510d5da8a46b0101304092dd2592e7dd73f2
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-09 15:43:30 -0700

    [LeetCode] RecoverBinarySearchString - Base cases working (2/2).

--- LeetCode/src/RecoverBinarySearchTree.java
+++ LeetCode/src/RecoverBinarySearchTree.java
@@ -1,6 +1,9 @@
 import com.leetcode.util.DebugUtil;
 import com.leetcode.util.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecoverBinarySearchTree {
     public static void main(String[] args) {
         TreeNode t1 = new TreeNode(1);
@@ -18,76 +21,132 @@ public class RecoverBinarySearchTree {
         t4.left = t2;
 
         recoverTree(t3);
-        DebugUtil.printTree(t1);
+        DebugUtil.printTree(t3);
     }
 
     public static void recoverTree(TreeNode root) {
-        recoverTreeRecursive(root, new InvalidNodes());
+//        recoverTreeRecursive(root, new InvalidNodes());
+        List<TreeNode> output = new ArrayList<>();
+        findInvalidNodes(root, null, output);
+        if (output.size() != 2) {
+            throw new RuntimeException("Error: output size is not 2");
+        }
+
+        swapNodeValues(output.get(0), output.get(1));
     }
 
-    private static void recoverTreeRecursive(TreeNode root, InvalidNodes invalidNodes) {
-        if (root == null) {
+    /**
+     * Algorithm:
+     * In-order traversal. Where node val is lower than previous node - invalid node.
+     * Record two instances of invalid node and return.
+     */
+    private static void findInvalidNodes(TreeNode node, TreeNode prevNode, List<TreeNode> invalidNodes) {
+        if (node == null){
             return;
         }
 
-        // Check Root
-        if ((root.left != null && root.val < root.left.val)
-                || (root.right != null && root.val > root.right.val)) {
-            invalidNodes.setInvalidNode(root);
-        }
-
-        if (invalidNodes.areBothInvalidNodesFound()) {
-            swapNodeVals(invalidNodes);
+        if (invalidNodes.size() > 2) {
             return;
         }
 
-        // Check left
-        if (root.left != null && root.left.val > root.val) {
-            invalidNodes.setInvalidNode(root.left);
+        findInvalidNodes(node.left, prevNode, invalidNodes);
+
+        boolean isInvalid = isInvalidNode(node, prevNode);
+        if (invalidNodes.size() == 0 && isInvalid) {
+            invalidNodes.add(prevNode);
         }
 
-        if (invalidNodes.areBothInvalidNodesFound()) {
-            swapNodeVals(invalidNodes);
-            return;
-        }
-
-        // Check right
-        if (root.right != null && root.right.val < root.val) {
-            invalidNodes.setInvalidNode(root.right);
-        }
-
-        if (invalidNodes.areBothInvalidNodesFound()) {
-            swapNodeVals(invalidNodes);
-            return;
-        }
-
-        recoverTreeRecursive(root.left, invalidNodes);
-        recoverTreeRecursive(root.right, invalidNodes);
-    }
-
-    private static class InvalidNodes {
-        public TreeNode firstNode;
-        public TreeNode secondNode;
-
-        public boolean areBothInvalidNodesFound() {
-            return firstNode != null && secondNode != null;
-        }
-
-        public void setInvalidNode(TreeNode node) {
-            if (firstNode == null) {
-                firstNode = node;
-            } else if (secondNode == null) {
-                secondNode = node;
+        if (invalidNodes.size() > 0 && isInvalid) {
+            if (invalidNodes.size() == 2) {
+                invalidNodes.set(1, node);
             } else {
-                throw new RuntimeException("[Error]: Both nodes are already defined.");
+                invalidNodes.add(node);
             }
         }
+        prevNode = node;
+
+        findInvalidNodes(node.right, prevNode, invalidNodes);
     }
 
-    private static void swapNodeVals(InvalidNodes invalidNodes) {
-        int tempVal = invalidNodes.firstNode.val;
-        invalidNodes.firstNode.val = invalidNodes.secondNode.val;
-        invalidNodes.secondNode.val = tempVal;
+    private static boolean isInvalidNode(TreeNode node, TreeNode prevNode) {
+        if (prevNode == null) {
+            return false;
+        }
+
+        return prevNode.val > node.val;
     }
+
+    private static void swapNodeValues(TreeNode first, TreeNode second) {
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+
+
+//    private static void recoverTreeRecursive(TreeNode root, InvalidNodes invalidNodes) {
+//        if (root == null) {
+//            return;
+//        }
+//
+//        // Check Root
+//        if ((root.left != null && root.val < root.left.val)
+//                || (root.right != null && root.val > root.right.val)) {
+//            invalidNodes.setInvalidNode(root);
+//        }
+//
+//        if (invalidNodes.areBothInvalidNodesFound()) {
+//            swapNodeVals(invalidNodes);
+//            return;
+//        }
+//
+//        // Check left
+//        if (root.left != null && root.left.val > root.val) {
+//            invalidNodes.setInvalidNode(root.left);
+//        }
+//
+//        if (invalidNodes.areBothInvalidNodesFound()) {
+//            swapNodeVals(invalidNodes);
+//            return;
+//        }
+//
+//        // Check right
+//        if (root.right != null && root.right.val < root.val) {
+//            invalidNodes.setInvalidNode(root.right);
+//        }
+//
+//        if (invalidNodes.areBothInvalidNodesFound()) {
+//            swapNodeVals(invalidNodes);
+//            return;
+//        }
+//
+//        recoverTreeRecursive(root.left, invalidNodes);
+//        recoverTreeRecursive(root.right, invalidNodes);
+//    }
+//
+//    private static class InvalidNodes {
+//        public TreeNode firstNode;
+//        public TreeNode secondNode;
+//
+//        public boolean areBothInvalidNodesFound() {
+//            return firstNode != null && secondNode != null;
+//        }
+//
+//        public void setInvalidNode(TreeNode node) {
+//            if (firstNode == null) {
+//                firstNode = node;
+//            } else if (secondNode == null) {
+//                secondNode = node;
+//            } else {
+//                throw new RuntimeException("[Error]: Both nodes are already defined.");
+//            }
+//        }
+//    }
+//
+//    private static void swapNodeVals(InvalidNodes invalidNodes) {
+//        int tempVal = invalidNodes.firstNode.val;
+//        invalidNodes.firstNode.val = invalidNodes.secondNode.val;
+//        invalidNodes.secondNode.val = tempVal;
+//    }
 
 }
