be79149a3cf17d290bbd0029c466913c414af331
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-23 16:04:21 -0700

    [LeetCode] BestTimeToSellStocks3 - Partial solution (206/214).
    Time limit exceeded.

--- LeetCode/src/BestTimeToBuyAndSellStock3.java
+++ LeetCode/src/BestTimeToBuyAndSellStock3.java
@@ -2,21 +2,29 @@ import com.leetcode.util.DebugUtil;
 
 public class BestTimeToBuyAndSellStock3 {
     public static void main(String[] args) {
-        int[] prices = { 3, 3, 5, 0, 0, 3, 1, 4 };
+//        int[] prices = { 3, 3, 5, 0, 0, 3, 1, 4 };
+//        System.out.println(maxProfit(prices));
+
+//        int[] prices = { 1, 2, 3, 4, 5 };
+//        System.out.println(maxProfit(prices));
+
+//        int[] prices = { 5, 4, 3, 2, 1 };
+//        System.out.println(maxProfit(prices));
+
+        int[] prices = { 1 };
         System.out.println(maxProfit(prices));
     }
 
     /**
      * DP algorithm:
-     * Let dp(i, j) = i is transaction number, and j is sell date,
-     * then,
-     * dp(i, j) = two choices
-     * a) Don't sell stock today - copy over value from previous day dp(i, j-1)
-     * b) Sell stock today - today's profit/loss (price(j) - price(k) + previous transaction dp(i-1, j-1)), where
-     * k = the day stock was bought (where k = 0 ... j-1)
-     *
-     * which gives us the recurrence formula:
-     * dp(i, j) = max(dp(i, j-1), price(j) - price(k) + dp(i-1, j-1)); k = 0...j-1
+     * t = transactions, dp rows
+     * d = transaction day index, dp cols
+     * DP Choices
+     * 1) No transaction - copy over previous day profit
+     * dp[t][d] = dp[t][d-1]
+     * 2) Transact on day d:
+     * max(price when selling stock on day d + max(-buying price on day x + max profit on previous transaction before day d)
+     * max(price(d) + max(-price(x) + dp(t-1)(x))); x = 0 ...d
      */
     public static int maxProfit(int[] prices) {
         if (prices.length <= 1) {
@@ -25,21 +33,18 @@ public class BestTimeToBuyAndSellStock3 {
 
         int[][] dp = new int[3][prices.length];
 
-//        for (int i = 1; i <= prices.length; i++) {
-//            for (int j = 1; j <= i-1; j++) {
-//                dp[i][j] = Math.max(dp[i][j-1], prices[j-1] - prices[i-1] + dp[i-1][j-1]); // price is zero index'd
-//            }
-//        }
-
-        for (int i = 1; i <= 2; i++) { // Num transactions
-            for (int j = 1; j < prices.length; j++) {
-                for (int k = 1; k <= j; k++) {
-                    dp[i][j] = Math.max(dp[i][j-1], prices[j] - prices[k] + dp[i-1][j-1]);
+        for (int t = 1; t <= 2; t++) {
+            for (int d = 1; d < prices.length; d++) {
+                int maxPreviousProfitCombo = Integer.MIN_VALUE;
+                for (int x = 0; x <= d; x++) {
+                    maxPreviousProfitCombo = Math.max(maxPreviousProfitCombo, -prices[x] + dp[t-1][x]);
                 }
+
+                dp[t][d] = Math.max(dp[t][d-1], prices[d] + maxPreviousProfitCombo);
             }
         }
 
-        DebugUtil.debugDpCache(dp, 3, prices.length);
+//        DebugUtil.debugDpCache(dp, 3, prices.length);
 
         return dp[2][prices.length - 1];
     }
