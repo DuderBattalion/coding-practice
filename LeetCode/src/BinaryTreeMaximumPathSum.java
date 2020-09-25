fba9bdbb5a9db17ebceb41cc816007b8ac9fcf42
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-24 17:31:52 -0700

    [LeetCode] BinaryTreeMaximumPathSum - Partial solution (65/93).

--- LeetCode/src/BinaryTreeMaximumPathSum.java
+++ LeetCode/src/BinaryTreeMaximumPathSum.java
@@ -15,42 +15,47 @@ public class BinaryTreeMaximumPathSum {
 //
 //        System.out.println(maxPathSum(node1));
 
-        TreeNode node1 = new TreeNode(-10);
-//        TreeNode node2 = new TreeNode(9);
-//        TreeNode node3 = new TreeNode(20);
-//        TreeNode node4 = new TreeNode(15);
-//        TreeNode node5 = new TreeNode(7);
-//
-//        node1.left = node2;
-//        node1.right = node3;
-//        node3.left = node4;
-//        node3.right = node5;
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(-2);
+        TreeNode node3 = new TreeNode(-3);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(-2);
+        TreeNode node7 = new TreeNode(-1);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node6.left = node7;
 
         System.out.println(maxPathSum(node1));
     }
 
     public static int maxPathSum(TreeNode root) {
-        return maxPathSum(root, Integer.MIN_VALUE, Integer.MIN_VALUE);
+        Result result = new Result();
+        result.maxSum = Integer.MIN_VALUE;
+
+        maxPathSum(root, result);
+
+        return result.maxSum;
     }
 
-    public static int maxPathSum(TreeNode root, int sum, int maxSum) {
+    public static int maxPathSum(TreeNode root, Result result) {
         if (root == null) {
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
-
-            return maxSum;
+            return 0;
         }
 
-        int leftSum = maxPathSum(root.left, Integer.MIN_VALUE, maxSum);
-        int rightSum = maxPathSum(root.right, Integer.MIN_VALUE, maxSum);
+        int leftSum = maxPathSum(root.left, result);
+        int rightSum = maxPathSum(root.right, result);
 
-        if (leftSum > maxSum) {
-            maxSum = leftSum;
+        if (leftSum > result.maxSum) {
+            result.maxSum = leftSum;
         }
 
-        if (rightSum > maxSum) {
-            maxSum = rightSum;
+        if (rightSum > result.maxSum) {
+            result.maxSum = rightSum;
         }
 
         // Cases
@@ -59,9 +64,6 @@ public class BinaryTreeMaximumPathSum {
         // sum = root + right
 
         int newSum = root.val;
-        if (sum > 0) {
-            newSum += sum;
-        }
 
         if (leftSum > 0) {
             newSum += leftSum;
@@ -71,10 +73,14 @@ public class BinaryTreeMaximumPathSum {
             newSum += rightSum;
         }
 
-        if (newSum > maxSum) {
-            maxSum = newSum;
+        if (newSum > result.maxSum) {
+            result.maxSum = newSum;
         }
 
-        return maxSum;
+        return newSum;
+    }
+
+    private static class Result {
+        public int maxSum;
     }
 }
