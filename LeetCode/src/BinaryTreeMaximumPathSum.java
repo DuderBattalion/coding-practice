38e22c1402265445687977da0fa024f091ea6901
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-24 17:46:04 -0700

    [LeetCode] BinaryTreeMaximumPathSum - Partial solution (71/93).

--- LeetCode/src/BinaryTreeMaximumPathSum.java
+++ LeetCode/src/BinaryTreeMaximumPathSum.java
@@ -15,21 +15,24 @@ public class BinaryTreeMaximumPathSum {
 //
 //        System.out.println(maxPathSum(node1));
 
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(-2);
-        TreeNode node3 = new TreeNode(-3);
-        TreeNode node4 = new TreeNode(1);
-        TreeNode node5 = new TreeNode(3);
-        TreeNode node6 = new TreeNode(-2);
-        TreeNode node7 = new TreeNode(-1);
+//        TreeNode node1 = new TreeNode(1);
+//        TreeNode node2 = new TreeNode(-2);
+//        TreeNode node3 = new TreeNode(-3);
+//        TreeNode node4 = new TreeNode(1);
+//        TreeNode node5 = new TreeNode(3);
+//        TreeNode node6 = new TreeNode(-2);
+//        TreeNode node7 = new TreeNode(-1);
+//
+//        node1.left = node2;
+//        node1.right = node3;
+//        node2.left = node4;
+//        node2.right = node5;
+//        node3.left = node6;
+//        node6.left = node7;
+//
+//        System.out.println(maxPathSum(node1));
 
-        node1.left = node2;
-        node1.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node3.left = node6;
-        node6.left = node7;
-
+        TreeNode node1 = new TreeNode(-3);
         System.out.println(maxPathSum(node1));
     }
 
@@ -44,7 +47,7 @@ public class BinaryTreeMaximumPathSum {
 
     public static int maxPathSum(TreeNode root, Result result) {
         if (root == null) {
-            return 0;
+            return Integer.MIN_VALUE;
         }
 
         int leftSum = maxPathSum(root.left, result);
