e221d237d8bd0e079f657a0a94c6f8b700f55ff4
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-30 15:09:11 -0700

    [LeetCode] LongestConsecutiveSequence - Fixing bugs.

--- LeetCode/src/LongestConsecutiveSequence.java
+++ LeetCode/src/LongestConsecutiveSequence.java
@@ -1,29 +1,29 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
-        int[] nums = { 100, 4, 200, 1, 3, 2 };
+//        int[] nums = { 100, 4, 200, 1, 3, 2 };
+        int[] nums = { 100, 2, 200, 1, 4, 3 };
+
         System.out.println(longestConsecutive(nums));
     }
 
     public static int longestConsecutive(int[] nums) {
-        Map<Integer, Integer> sequenceCount = new HashMap<>();
+        Map<Integer, Integer> numCount = new HashMap<>();
         for (int num: nums) {
-            sequenceCount.put(num, 1);
+            numCount.put(num, 1);
         }
 
         int maxSequenceLength = Integer.MIN_VALUE;
         for (int num: nums) {
             int count = 0;
-            while (sequenceCount.containsKey(num)) {
-                count += sequenceCount.get(num);
-                sequenceCount.remove(num);
-
-                num--;
+            if (numCount.containsKey(num)) {
+                count += numCount.get(num);
+                count += accumulateLowerNumCounts(num - 1, numCount);
             }
+
+            numCount.put(num, count);
 
             if (count > maxSequenceLength) {
                 maxSequenceLength = count;
@@ -31,5 +31,17 @@ public class LongestConsecutiveSequence {
         }
 
         return maxSequenceLength;
+    }
+
+    private static int accumulateLowerNumCounts(int num, Map<Integer, Integer> numCount) {
+        int count = 0;
+        while (numCount.containsKey(num)) {
+            count += numCount.get(num);
+            numCount.remove(num);
+
+            num--;
+        }
+
+        return count;
     }
 }
