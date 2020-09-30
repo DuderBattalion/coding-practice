012c290add19a1d8cd2be45b3258afd39624baed
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-09-30 15:13:27 -0700

    [LeetCode] LongestConsecutiveSequence - Adding solution.

--- LeetCode/src/LongestConsecutiveSequence.java
+++ LeetCode/src/LongestConsecutiveSequence.java
@@ -4,12 +4,30 @@ import java.util.Map;
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
 //        int[] nums = { 100, 4, 200, 1, 3, 2 };
-        int[] nums = { 100, 2, 200, 1, 4, 3 };
+//        int[] nums = { 100, 2, 200, 1, 4, 3 };
+//        int[] nums = {  };
+        int[] nums = { 100 };
 
         System.out.println(longestConsecutive(nums));
     }
 
+    /**
+     * Algorithm:
+     * Maintain a numCount map for each number.
+     * Loop through each number in nums array.
+     * For each number, decrement value and see if lower adjacent numbers exist. Calculate
+     * count of such lower nums, and update accumulated count in map.
+     * Remove lower numbers that were accumulated.
+     *
+     * Return max of such a count for longest consecutive sequence.
+     * @param nums
+     * @return
+     */
     public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
         Map<Integer, Integer> numCount = new HashMap<>();
         for (int num: nums) {
             numCount.put(num, 1);
