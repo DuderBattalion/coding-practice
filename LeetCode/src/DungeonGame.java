ecc35c31aeed06f23be25aca9bb9fccde40cf381
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-10-13 21:30:43 -0700

    [LeetCode] DungeonGame - Base case working (1/1).

--- LeetCode/src/DungeonGame.java
+++ LeetCode/src/DungeonGame.java
@@ -1,6 +1,12 @@
 public class DungeonGame {
     public static void main(String[] args) {
+        int[][] dungeon = {
+                { -2, -3, 3},
+                { -5, -10, 1},
+                {10, 30, -5 }
+        };
 
+        System.out.println(calculateMinimumHP(dungeon));
     }
 
     /**
@@ -14,9 +20,27 @@ public class DungeonGame {
      * @param dungeon
      * @return
      */
-    public int calculateMinimumHP(int[][] dungeon) {
+    public static int calculateMinimumHP(int[][] dungeon) {
         int[][] dp = new int[dungeon.length+1][dungeon.length+1];
 
+        for (int i = dungeon.length - 1; i >= 0; i--) {
+            for (int j = dungeon.length - 1; j >= 0; j--) {
+                int rightVal = 0, downVal = 0;
+                if (j + 1 < dungeon.length) {
+                    rightVal = dp[i][j+1];
+                }
 
+                if (i + 1 < dungeon.length) {
+                    downVal = dp[i+1][j];
+                }
+
+                dp[i][j] = Math.min(rightVal - dungeon[i][j], downVal - dungeon[i][j]);
+                if (dp[i][j] <= 0) {
+                    dp[i][j] = 1; // Note: Negative values show excess health which is not required. Anchor at 1.
+                }
+            }
+        }
+
+        return dp[0][0] + 1;
     }
 }
