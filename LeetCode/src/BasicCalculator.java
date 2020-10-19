e72a82aa9892b792e7844119ebd01f100286ff20
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-10-19 07:21:15 -0700

    [LeetCode] BasicCalculator - Refactoring to use stacks to manage equation states.

--- LeetCode/src/BasicCalculator.java
+++ LeetCode/src/BasicCalculator.java
@@ -1,14 +1,134 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class BasicCalculator {
     public static void main(String[] args) {
-        System.out.println(calculate("1+(2+3)"));
-        System.out.println(calculate("(1+(2+3))-4"));
+//        System.out.println(calculate("1+(2+3)"));
+//        System.out.println(calculate("(1+(2+3))-4"));
         System.out.println(calculate("(1+(4+5+2)-3)+(6+8)"));
         System.out.println(calculate("(1+3)+(6+8)"));
         System.out.println(calculate(" 2-1 + 2 "));
-
+//
         System.out.println(calculate("4-5+2"));
         System.out.println(calculate("(7)-(0)+(4)"));
+        System.out.println(calculate("2-4-(8+2-6+(8+4-(1)+8-10))"));
     }
+
+    /**
+     * Algorithm:
+     * Keep calculating results one by one. Start with 0 + .
+     *
+     * When we hit bracket, "(" - push state on stack (sum and sign before bracket)
+     * Reset sum to 0, sign to '+', and process expression inside bracket.
+     *
+     * When we hit bracket ")", pop state from stack and add current state.
+     *
+     * Keep doing this till end of string.
+     */
+    public static int calculate(String s) {
+        s = s.replaceAll(" ", "");
+
+        int sum = 0, sign = 1;
+        Deque<EquationState> equationStates = new ArrayDeque<>();
+
+        int i = 0;
+        while (i < s.length()) {
+            Token token = getToken(s, i);
+            if (token.type.equals("num")) { // TODO - make enum
+                int num = Integer.parseInt(token.val);
+                if (num < 0) {
+                    throw new RuntimeException("[Error]: Number cannot be negative. num: " + num);
+                }
+
+                sum += num * sign;
+            } else if (token.type.equals("sign")) {
+                if (token.val.equals("+")) {
+                    sign = 1;
+                } else {
+                    sign = -1;
+                }
+            } else if (token.type.equals("bracketOpen")) {
+                EquationState state = new EquationState(sum, sign);
+                equationStates.push(state);
+
+                sum = 0;
+                sign = 1;
+            } else if (token.type.equals("bracketClose")) {
+                EquationState state = equationStates.pop();
+                sum = state.sum + sum * state.sign;
+            }
+
+            i += token.val.length();
+        }
+
+        return sum;
+    }
+
+    private static class EquationState {
+        public int sum;
+        public int sign;
+
+        public EquationState(int sum, int sign) {
+            this.sum = sum;
+            this.sign = sign;
+        }
+    }
+
+    private static Token getToken(String s, int i) {
+        char tokenChar = s.charAt(i);
+
+        Token token;
+        if (isNumChar(tokenChar)) {
+            String numStr = getNumStr(s, i);
+            token = new Token(numStr, "num");
+        } else if (tokenChar == '+' || tokenChar == '-'){
+            token = new Token(String.valueOf(tokenChar), "sign");
+        } else if (tokenChar == '(') {
+            token = new Token(String.valueOf(tokenChar), "bracketOpen");
+        } else if (tokenChar == ')') {
+            token = new Token(String.valueOf(tokenChar), "bracketClose");
+        } else {
+            throw new RuntimeException("[Error]: Invalid token character: " + tokenChar);
+        }
+
+        return token;
+    }
+
+    private static class Token {
+        String val;
+        String type;
+
+        public Token(String val, String type) {
+            this.val = val;
+            this.type = type;
+        }
+    }
+
+    private static String getNumStr(String s, int i) {
+    if (s.charAt(i) == '(') {
+        return "(";
+    }
+
+    StringBuilder num = new StringBuilder();
+
+    // Handle negative value
+    if (s.charAt(i) == '-') {
+        num.append("-");
+        i++;
+    }
+
+    while (i < s.length() && isNumChar(s.charAt(i))) {
+        num.append(s.charAt(i));
+        i++;
+    }
+
+    return num.toString();
+}
+
+private static boolean isNumChar(char token) {
+    int numToken = (token - '0');
+    return numToken >= 0 && numToken <= 9;
+}
 
     /**
      * Algorithm:
@@ -33,111 +153,111 @@ public class BasicCalculator {
      * - Keep doing this till index is at end of string.
      *
      */
-    public static int calculate(String s) {
-        s = s.replaceAll(" ", "");
-
-        int subResult;
-        int[] globalIndex = new int[1];
-        do {
-            subResult = recursiveCalculate(s, 0, globalIndex);
-            if (globalIndex[0] >= s.length()) {
-                break;
-            }
-
-            s = subResult + s.substring(globalIndex[0]);
-            globalIndex[0] = 0;
-        } while (true);
-
-        return subResult;
-    }
-
-    private static int recursiveCalculate(String s, int i, int[] globalIndex) {
-        if (i >= s.length()) {
-            globalIndex[0] = i;
-            return 0;
-        }
-
-        String leftNumStr = getNumStr(s, i);
-        if (leftNumStr.equals("(")) {
-            int subResult = recursiveCalculate(s, i+1, globalIndex);
-            leftNumStr = String.valueOf(subResult);
-            i = globalIndex[0];
-        } else {
-            i += leftNumStr.length();
-        }
-
-        if (i >= s.length()) {
-            globalIndex[0] = i;
-            return Integer.parseInt(leftNumStr);
-        }
-
-        char operand = s.charAt(i);
-        i++;
-        if (operand == ')') {
-            globalIndex[0] = i;
-            return Integer.parseInt(leftNumStr);
-        }
-
-//        char operand;
+//    public static int calculate(String s) {
+//        s = s.replaceAll(" ", "");
+//
+//        int subResult;
+//        int[] globalIndex = new int[1];
 //        do {
-//            operand = s.charAt(i);
+//            subResult = recursiveCalculate(s, 0, globalIndex);
+//            if (globalIndex[0] >= s.length()) {
+//                break;
+//            }
+//
+//            s = subResult + s.substring(globalIndex[0]);
+//            globalIndex[0] = 0;
+//        } while (true);
+//
+//        return subResult;
+//    }
+//
+//    private static int recursiveCalculate(String s, int i, int[] globalIndex) {
+//        if (i >= s.length()) {
+//            globalIndex[0] = i;
+//            return 0;
+//        }
+//
+//        String leftNumStr = getNumStr(s, i);
+//        if (leftNumStr.equals("(")) {
+//            int subResult = recursiveCalculate(s, i+1, globalIndex);
+//            leftNumStr = String.valueOf(subResult);
+//            i = globalIndex[0];
+//        } else {
+//            i += leftNumStr.length();
+//        }
+//
+//        if (i >= s.length()) {
+//            globalIndex[0] = i;
+//            return Integer.parseInt(leftNumStr);
+//        }
+//
+//        char operand = s.charAt(i);
+//        i++;
+//        if (operand == ')') {
+//            globalIndex[0] = i;
+//            return Integer.parseInt(leftNumStr);
+//        }
+//
+////        char operand;
+////        do {
+////            operand = s.charAt(i);
+////            i++;
+////        } while (operand == ')' && i < s.length());
+//
+//        if (i >= s.length()) {
+//            globalIndex[0] = i;
+//            return Integer.parseInt(leftNumStr);
+//        }
+//
+//        String rightNumStr = getNumStr(s, i);
+//        if (rightNumStr.equals("(")) {
+//            int subResult = recursiveCalculate(s, i+1, globalIndex);
+//            rightNumStr = String.valueOf(subResult);
+//
+//            i = globalIndex[0];
+//        } else {
+//            i += rightNumStr.length();
+//        }
+//
+//        int leftNum = Integer.parseInt(leftNumStr);
+//        int rightNum = Integer.parseInt(rightNumStr);
+//
+//        int result;
+//        if (operand == '+') {
+//            result = leftNum + rightNum;
+//        } else {
+//            result = leftNum - rightNum;
+//        }
+//
+//        globalIndex[0] = i;
+//        return result;
+//    }
+//
+//    private static String getNumStr(String s, int i) {
+//        if (s.charAt(i) == '(') {
+//            return "(";
+//        }
+//
+//        StringBuilder num = new StringBuilder();
+//
+//        // Handle negative value
+//        if (s.charAt(i) == '-') {
+//            num.append("-");
 //            i++;
-//        } while (operand == ')' && i < s.length());
-
-        if (i >= s.length()) {
-            globalIndex[0] = i;
-            return Integer.parseInt(leftNumStr);
-        }
-
-        String rightNumStr = getNumStr(s, i);
-        if (rightNumStr.equals("(")) {
-            int subResult = recursiveCalculate(s, i+1, globalIndex);
-            rightNumStr = String.valueOf(subResult);
-
-            i = globalIndex[0];
-        } else {
-            i += rightNumStr.length();
-        }
-
-        int leftNum = Integer.parseInt(leftNumStr);
-        int rightNum = Integer.parseInt(rightNumStr);
-
-        int result;
-        if (operand == '+') {
-            result = leftNum + rightNum;
-        } else {
-            result = leftNum - rightNum;
-        }
-
-        globalIndex[0] = i;
-        return result;
-    }
-
-    private static String getNumStr(String s, int i) {
-        if (s.charAt(i) == '(') {
-            return "(";
-        }
-
-        StringBuilder num = new StringBuilder();
-
-        // Handle negative value
-        if (s.charAt(i) == '-') {
-            num.append("-");
-            i++;
-        }
-
-        while (i < s.length() && isNumChar(s.charAt(i))) {
-            num.append(s.charAt(i));
-            i++;
-        }
-
-        return num.toString();
-    }
-
-    private static boolean isNumChar(char token) {
-        int numToken = (token - '0');
-        return numToken >= 0 && numToken <= 9;
-    }
+//        }
+//
+//        while (i < s.length() && isNumChar(s.charAt(i))) {
+//            num.append(s.charAt(i));
+//            i++;
+//        }
+//
+//        return num.toString();
+//    }
+//
+//    private static boolean isNumChar(char token) {
+//        int numToken = (token - '0');
+//        return numToken >= 0 && numToken <= 9;
+//    }
 
 //    private static SubResult recursiveCalculate(String s, SubResult result) {
 //        if (result.index >= s.length()) {
