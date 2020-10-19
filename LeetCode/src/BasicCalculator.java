0d4f0611a7d726644d84ff4e0564b96da3097139
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-10-18 21:52:34 -0700

    [LeetCode] BasicCalculator - WIP implementation.

--- LeetCode/src/BasicCalculator.java
+++ LeetCode/src/BasicCalculator.java
@@ -1,11 +1,47 @@
 public class BasicCalculator {
     public static void main(String[] args) {
-        System.out.println(calculate("1+(2+3)"));
+//        System.out.println(calculate("1+(2+3)"));
+//        System.out.println(calculate("(1+(2+3))-4"));
+//        System.out.println(calculate("(1+(4+5+2)-3)+(6+8)"));
+        System.out.println(calculate("(1+3)+(6+8)"));
     }
 
+    /**
+     * Algorithm:
+     * Ex: 1 + (2 + 3) - 4
+     * - Step 1: Pick first num
+     * - Pick 1st token
+     * - If token is '(', then do recursive calculate sub result.
+     * - result += subresult.sum (or normal num)
+     * - Move index to subresult.index + 1 (or index + 1)
+     * -
+     * - Step 2
+     * - Pick operand '+' or '-'
+     * -
+     * - Step 3
+     * - Pick token
+     * - Same steps as Step 1
+     * -
+     * - Step 4
+     * - Do calculations: firstNum, operand, secondNum
+     * - Move index to after secondNum
+     * -
+     * - Keep doing this till index is at end of string.
+     *
+     */
     public static int calculate(String s) {
-        SubResult result = recursiveCalculate(s, new SubResult(0, 0));
-        return result.result;
+        int result = 0;
+        int index = 0;
+
+        do {
+            SubResult subResult = recursiveCalculate(s, new SubResult(0, 0));
+            result = subResult.result;
+            index = subResult.index;
+
+            s = result + s.substring(index);
+        } while (s.length() != 1);
+
+        return result;
     }
 
     private static SubResult recursiveCalculate(String s, SubResult result) {
@@ -16,11 +52,9 @@ public class BasicCalculator {
         char token = s.charAt(result.index);
         if (token == '(') {
             SubResult subResult = recursiveCalculate(s, new SubResult(0, result.index+1));
-            result.index = subResult.index;
+            result.index = subResult.index + 1;
             result.result += subResult.result;
-        } else if (token == ')') {
-            result.index += 1;
-        }  else {
+        } else {
             // TODO - more intelligent parse to get multi-digit number
             // Assuming single digit for now.
             // Also, assuming no spaces. Have to strip out spaces.
@@ -32,7 +66,7 @@ public class BasicCalculator {
             if (rightNumChar == '(') {
                 SubResult subResult = recursiveCalculate(s, new SubResult(0, result.index+3));
                 rightNum = subResult.result;
-                result.index = subResult.index;
+                result.index = subResult.index + 1;
             } else {
                 rightNum = Character.getNumericValue(s.charAt(result.index+2));
                 result.index = result.index + 3;
@@ -41,11 +75,12 @@ public class BasicCalculator {
             if (operator == '+') {
                 result.result += (leftNum + rightNum);
             } else {
-                result.result += (rightNum - leftNum);
+                result.result += (leftNum - rightNum);
             }
         }
 
-        return recursiveCalculate(s, result);
+//        s = result.result + s.substring(result.index);
+        return result;
     }
 
     private static class SubResult {
