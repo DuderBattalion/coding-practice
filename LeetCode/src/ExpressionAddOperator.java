99c1a7f0cf6a67ba81820ef57312b0eb603d5558
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-10-24 10:34:43 -0700

    [LeetCode] ExpressionAddOperator - Fixing a bug in multiplying.
    Previous operand being passed into next recursion after
    multiplying was incorrect.

--- LeetCode/src/ExpressionAddOperator.java
+++ LeetCode/src/ExpressionAddOperator.java
@@ -1,8 +1,13 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExpressionAddOperator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ScriptException {
 //        String num = "123";
 //        int target = 6;
 
@@ -12,16 +17,39 @@ public class ExpressionAddOperator {
 //        String num = "105";
 //        int target = 5;
 
-        String num = "00";
-        int target = 0;
+//        String num = "00";
+//        int target = 0;
 
 //        String num = "3456237490";
 //        int target = 9191;
+
+//        String num = "123456789";
+//        int target = 45;
+
+        String num = "123456789";
+        int target = 45;
 
         List<String> output = addOperators(num, target);
         for (String row: output) {
             System.out.println(row);
         }
+
+        System.out.println();
+        System.out.println("=======================");
+
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        for (String row: output) {
+            int result = (int) engine.eval(row);
+            if (result != target) {
+                System.out.println(row);
+            } else {
+                System.out.println(target);
+            }
+        }
+
+//        Set<String> test = new HashSet<>(output);
+//        System.out.println(String.format("List has %d elements. Set has %d elements", output.size(), test.size()));
     }
 
     public static List<String> addOperators(String num, int target) {
@@ -72,17 +100,17 @@ public class ExpressionAddOperator {
         newExpression = expression + "*";
         double newValue = value - prevOperandValue + (prevOperandValue * operandValue);
         recursiveAddOperators(num, target, newExpression + operand,
-                newValue, i+1, operand, output);
+                newValue, i+1, String.valueOf(prevOperandValue * operandValue), output);
 
         // No operator added - include next character in operand
         String mergeOperand = prevOperand + operand;
         newExpression = expression + operand;
         double mergeOperandValue = Double.parseDouble(mergeOperand);
-        value = (value - prevOperandValue) + mergeOperandValue;
+        newValue = (value - prevOperandValue) + mergeOperandValue;
 
         // To get around results like 1*05 - where 05 is not eligible
         if (mergeOperand.charAt(0) != '0') {
-            recursiveAddOperators(num, target, newExpression, value, i+1, mergeOperand, output);
+            recursiveAddOperators(num, target, newExpression, newValue, i+1, mergeOperand, output);
         }
     }
 }
