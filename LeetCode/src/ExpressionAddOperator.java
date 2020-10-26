0d9a6227e0f651b6ea088c7ee9f4380de6e4cf08
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-10-25 21:24:19 -0700

    [LeetCode] ExpressionAddOperator - Adding solution.

--- LeetCode/src/ExpressionAddOperator.java
+++ LeetCode/src/ExpressionAddOperator.java
@@ -20,40 +20,43 @@ public class ExpressionAddOperator {
 //        String num = "00";
 //        int target = 0;
 
-//        String num = "3456237490";
-//        int target = 9191;
+        String num = "3456237490";
+        int target = 9191;
 
 //        String num = "123456789";
 //        int target = 45;
 
-        String num = "123456789";
-        int target = 45;
+//        String num = "123456789";
+//        int target = 45;
 
         List<String> output = addOperators(num, target);
         for (String row: output) {
             System.out.println(row);
         }
-
-        System.out.println();
-        System.out.println("=======================");
-
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        for (String row: output) {
-            int result = (int) engine.eval(row);
-            if (result != target) {
-                System.out.println(row);
-            } else {
-                System.out.println(target);
-            }
-        }
-
-//        Set<String> test = new HashSet<>(output);
-//        System.out.println(String.format("List has %d elements. Set has %d elements", output.size(), test.size()));
     }
 
+    /**
+     * Algorithm:
+     * Scan from left to right. Recurse through the following cases:
+     * - add
+     * - subtract
+     * - multiply
+     *
+     * Special cases to consider
+     * - For first operand starting at i = 0, no operators, simply add value
+     * - For multi-character numbers, go from i ... j, with j from i to num.length() - and recurse
+     * - For multiplication operator, revert the last operation: ex: 1 + 2 (=3), but this changes on
+     * 1+2*3 - since multiplication takes priority. So revert last operation (-2).
+     * @return
+     */
     public static List<String> addOperators(String num, int target) {
+        List<String> output = new ArrayList<>();
+        if (num.isEmpty()) {
+            return output;
+        }
 
+        recursiveAddOperators(num, target, output, 0, "", 0, 0);
+        return output;
     }
 
     private static void recursiveAddOperators(String num, int target, List<String> output, int i,
@@ -66,85 +69,28 @@ public class ExpressionAddOperator {
             return;
         }
 
-        int digit = Character.getNumericValue(num.charAt(i));
+        for (int j = i; j < num.length(); j++) {
+            // Skip multi-character sequences that start with "0" - ex: 1 * 05
+            if (j != i && num.charAt(i) == '0') {
+                break;
+            }
 
-        // Add "+"
-        recursiveAddOperators(num, target, output, i+1, expression + "+" + digit,
-                sum + digit, digit);
+            long operand = Long.parseLong(num.substring(i, j + 1));
 
-        // Subtract "-"
-        recursiveAddOperators(num, target, output, i+1, expression + "-" + digit,
-                sum - digit, digit);
+            if (i == 0) {
+                recursiveAddOperators(num, target, output, j + 1,
+                        expression + operand, operand, operand);
+            } else {
+                recursiveAddOperators(num, target, output, j + 1,
+                        expression + "+" + operand, sum + operand, operand);
 
-        // Multiply "*"
-        long newSum = (sum - prevOperand) + (prevOperand * digit);
-        recursiveAddOperators(num, target, output, i+1, expression + "*" + digit, newSum,
-                prevOperand * digit);
+                recursiveAddOperators(num, target, output, j + 1,
+                        expression + "-" + operand, sum - operand, -operand);
 
-        // Add next digit: ex - "1" to "12"
-        long newOperand = Long.parseLong(String.valueOf(prevOperand) + String.valueOf(digit));
-
+                long newSum = sum - prevOperand + (prevOperand * operand);
+                recursiveAddOperators(num, target, output, j+1,
+                        expression + "*" + operand, newSum, prevOperand * operand);
+            }
+        }
     }
-
-//    public static List<String> addOperators(String num, int target) {
-//        List<String> output = new ArrayList<>();
-//        if (num.length() == 1) {
-//            if (Integer.parseInt(num) == target) {
-//                output.add(num);
-//            }
-//
-//            return output;
-//        }
-//
-//        String expression = num.substring(0, 1);
-//        int value = Integer.parseInt(expression);
-//
-//        recursiveAddOperators(num, target, expression, value, 1,
-//                expression, output);
-//
-//        return output;
-//    }
-//
-//    private static void recursiveAddOperators(String num, int target, String expression,
-//                                              long value, int i, String prevOperand,
-//                                              List<String> output) {
-//        // Checking expression length to get around num = "00", while
-//        // expression is "0" - which apparently is not accepted as
-//        // an answer - *commence eye roll* -
-//        if (value == target && expression.length() >= num.length() && i == num.length()) {
-//            output.add(expression);
-//        }
-//
-//        if (i >= num.length()) {
-//            return;
-//        }
-//
-//        String operand = String.valueOf(num.charAt(i));
-//        long operandValue = Long.parseLong(operand);
-//        long prevOperandValue = Long.parseLong(prevOperand);
-//
-//        String newExpression = expression + "+";
-//        recursiveAddOperators(num, target, newExpression + operand,
-//                value + operandValue, i+1, operand, output);
-//
-//        newExpression = expression + "-";
-//        recursiveAddOperators(num, target, newExpression + operand,
-//                value - operandValue, i+1, "-" + operand, output);
-//
-//        newExpression = expression + "*";
-//        long newValue = value - prevOperandValue + (prevOperandValue * operandValue);
-//        recursiveAddOperators(num, target, newExpression + operand,
-//                newValue, i+1, String.valueOf(prevOperandValue * operandValue), output);
-//
-//        // No operator added - include next character in operand
-//        String mergeOperand = prevOperand + operand;
-//        newExpression = expression + operand;
-//        long mergeOperandValue = Long.parseLong(mergeOperand);
-//        newValue = (value - prevOperandValue) + mergeOperandValue;
-//
-//        // To get around results like 1*05 - where 05 is not eligible
-//        if (mergeOperand.charAt(0) != '0') {
-//            recursiveAddOperators(num, target, newExpression, newValue, i+1, mergeOperand, output);
-//        }
-//    }
 }
