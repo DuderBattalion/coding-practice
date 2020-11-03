97c544d6f2429dd4e4432c843f8d93382ce1c27e
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-11-03 06:12:46 -0800

    [LeetCode] CreateMaximumNumber - WIP implementation.
    Adding mergeMaximumNumber function. Takes two int arrays and
    returns maximum number from combining all digits while preserving
    order.

--- LeetCode/src/CreateMaximumNumber.java
+++ LeetCode/src/CreateMaximumNumber.java
@@ -3,11 +3,19 @@ import java.util.Deque;
 
 public class CreateMaximumNumber {
     public static void main(String[] args) {
-        int[] nums1 = { 9, 1, 2, 5, 8, 3 };
+//        int[] nums1 = { 9, 1, 2, 5, 8, 3 };
+//
+//        int[] maxNums1 = getMaximumNumber(nums1, 3);
+//        for (int num: maxNums1) {
+//            System.out.print(num + " ");
+//        }
 
-        int[] maxNums1 = getMaximumNumber(nums1, 3);
-        for (int num: maxNums1) {
-            System.out.print(num + " ");
+        int[] nums1 = { 6, 7, 8 };
+        int[] nums2 = { 9, 7, 2, 9 };
+
+        int[] mergeNum = mergeMaximumNumbers(nums1, nums2);
+        for (int num: mergeNum) {
+            System.out.print(num + ", ");
         }
     }
 
@@ -43,5 +51,74 @@ public class CreateMaximumNumber {
         }
 
         return numArray;
+    }
+
+    private static int[] mergeMaximumNumbers(int[] nums1, int[] nums2) {
+        int[] mergedNum = new int[nums1.length + nums2.length];
+        int index1 = 0, index2 = 0, mergeIndex = 0;
+
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1] > nums2[index2]) {
+                mergedNum[mergeIndex] = nums1[index1];
+                index1++;
+            } else if (nums2[index2] > nums1[index1]) {
+                mergedNum[mergeIndex] = nums2[index2];
+                index2++;
+            } else {
+                // Equal values - Look ahead to pick value with greater
+                // upcoming number
+                // Ex: { 6, 7 } and { 6, 0, 4 } - pick 6 from { 6, 7 }
+                boolean isFirst = isFirstNumBetter(nums1, index1, nums2, index2);
+                if (isFirst) {
+                    mergedNum[mergeIndex] = nums1[index1];
+                    index1++;
+                } else {
+                    mergedNum[mergeIndex] = nums2[index2];
+                    index2++;
+                }
+            }
+
+            mergeIndex++;
+        }
+
+        while (index1 < nums1.length) {
+            mergedNum[mergeIndex] = nums1[index1];
+            index1++;
+            mergeIndex++;
+        }
+
+        while (index2 < nums2.length) {
+            mergedNum[mergeIndex] = nums2[index2];
+            index2++;
+            mergeIndex++;
+        }
+
+        return mergedNum;
+    }
+
+    private static boolean isFirstNumBetter(int[] nums1, int index1,
+                                            int[] nums2, int index2) {
+        Boolean isFirstBetter = null;
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1] > nums2[index2]) {
+                isFirstBetter = true;
+                break;
+            } else if (nums2[index2] > nums1[index1]) {
+                isFirstBetter = false;
+                break;
+            }
+
+            index1++;
+            index2++;
+        }
+
+        // If ran out of digits on one of the numbers, with previous
+        // digits equal (ex: 3 3 and 3 3 4), stick with the one with
+        // longer digits
+        if (isFirstBetter == null) {
+            isFirstBetter = index1 < nums1.length;
+        }
+
+        return isFirstBetter;
     }
 }
