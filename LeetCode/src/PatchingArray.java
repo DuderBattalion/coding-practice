a1c5d9fbaf266ef3034bba5fb86094496398a570
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-11-11 13:05:11 -0800

    [LeetCode] PatchingArray - WIP implementation.
    Adding calculateNumberSums() function.

--- LeetCode/src/PatchingArray.java
+++ LeetCode/src/PatchingArray.java
@@ -6,16 +6,36 @@ public class PatchingArray {
         int[] nums = { 1, 2, 3, 4 };
 
         // Test block
-        List<List<Integer>> numberSets = generatePowerSet(nums);
-        for (List<Integer> numberSet: numberSets) {
-            for (int num: numberSet) {
-                System.out.print(num + ",");
-            }
 
-            System.out.println();
-        }
+        // Test - generatePowerSet(nums);
+//        List<List<Integer>> numberSets = generatePowerSet(nums);
+//        for (List<Integer> numberSet: numberSets) {
+//            for (int num: numberSet) {
+//                System.out.print(num + ",");
+//            }
+//
+//            System.out.println();
+//        }
+//
+//        System.out.println(numberSets.size());
 
-        System.out.println(numberSets.size());
+        // Test - calculateNumberSums()
+        List<List<Integer>> numberSets = new ArrayList<>();
+
+        List<Integer> numberSet1 = new ArrayList<>();
+        numberSet1.add(1);
+        numberSet1.add(2);
+        numberSet1.add(3);
+
+        List<Integer> numberSet2 = new ArrayList<>();
+        numberSet2.add(4);
+        numberSet2.add(5);
+        numberSet2.add(6);
+
+        numberSets.add(numberSet1);
+        numberSets.add(numberSet2);
+
+        System.out.println(calculateNumberSums(numberSets));
     }
 
     /**
@@ -28,22 +48,22 @@ public class PatchingArray {
      * Calculate new number sums, and update number cover.
      * Keep doing this until all number sums are covered till N.
      */
-    public static int minPatches(int[] nums, int n) {
-        List<List<Integer>> numberSets = generatePowerSet(nums);
-        List<Integer> numberSums = calculateNumberSums(numberSets);
-
-        int patchCount = 0;
-        int smallestMissingNumber = getSmallestMissingNumber(numberSums, n);
-        while (smallestMissingNumber > 0) {
-            numberSets = extendNumberSets(numberSets, smallestMissingNumber);
-            numberSums = calculateNumberSums(numberSets);
-            patchCount++;
-
-            smallestMissingNumber = getSmallestMissingNumber(numberSums, n);
-        }
-
-        return patchCount;
-    }
+//    public static int minPatches(int[] nums, int n) {
+//        List<List<Integer>> numberSets = generatePowerSet(nums);
+//        List<Integer> numberSums = calculateNumberSums(numberSets);
+//
+//        int patchCount = 0;
+//        int smallestMissingNumber = getSmallestMissingNumber(numberSums, n);
+//        while (smallestMissingNumber > 0) {
+//            numberSets = extendNumberSets(numberSets, smallestMissingNumber);
+//            numberSums = calculateNumberSums(numberSets);
+//            patchCount++;
+//
+//            smallestMissingNumber = getSmallestMissingNumber(numberSums, n);
+//        }
+//
+//        return patchCount;
+//    }
 
     private static List<List<Integer>> generatePowerSet(int[] nums) {
         List<List<Integer>> powerSet = new ArrayList<>();
@@ -66,5 +86,20 @@ public class PatchingArray {
         }
 
         return powerSet;
+    }
+
+    private static List<Integer> calculateNumberSums(List<List<Integer>> numberSets) {
+        List<Integer> numberSums = new ArrayList<>();
+
+        for (List<Integer> numberSet: numberSets) {
+            int count = 0;
+            for (int num: numberSet) {
+                count += num;
+            }
+
+            numberSums.add(count);
+        }
+
+        return numberSums;
     }
 }
