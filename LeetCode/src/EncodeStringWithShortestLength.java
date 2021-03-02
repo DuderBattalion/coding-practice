83d7036f0e5107e3c8b466ba55616a0d6f4ad05d
Anurag Patnaik <anurag.patnaik@gmail.com>
2021-03-02 15:39:12 -0800

    [LeetCode] EncodeStringWithShortestLength - Base case (3/4).

--- LeetCode/src/EncodeStringWithShortestLength.java
+++ LeetCode/src/EncodeStringWithShortestLength.java
@@ -1,84 +1,85 @@
-import com.leetcode.util.SuffixArray;
+import java.math.BigInteger;
+import java.util.*;
 
-import java.util.regex.Pattern;
-
-public class EncodeStringWithShortestLength {
-
-    /**
-     * Algorithm
-     * Build suffix array
-     * For each substring, check if
-     * - Has LRS
-     * - Is composed of LRS x x repetitions
-     * - If so, compress it
-     * Keep doing this till no change.
-     */
-    public static String encode(String s) {
-        String encodedString = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String substring = s.substring(i, j+1);
-                String lrs = findLrs(substring);
-                if (lrs.length() < 2) {
-                    continue;
-                }
-
-                int repetition = calculateLrsRepetition(substring, lrs);
-                if (repetition > 1) {
-                    String encodedLrs = encode(lrs);
-                    String encodedSubstring = String.format("%d[%s]", repetition, encodedLrs);
-
-                    encodedString = s.replaceAll(Pattern.quote(substring), encodedSubstring);
-                    break;
-                }
-            }
-        }
-
-        if (encodedString.isEmpty() || encodedString.length() >= s.length()) {
-            encodedString = s;
-        } else {
-            encodedString = encode(encodedString);
-        }
-
-        return encodedString;
-    }
-
-    private static String findLrs(String s) {
-        SuffixArray suffixArray = new SuffixArray(s);
-
-        int maxLcp = Integer.MIN_VALUE;
-        String lrs = "";
-        for (int i = 1; i < suffixArray.length(); i++) {
-            int lcp = suffixArray.lcp(i);
-            if (lcp > maxLcp) {
-                maxLcp = lcp;
-                lrs = suffixArray.select(i).substring(0, lcp);
-            }
-        }
-
-        return lrs;
-    }
-
-    private static int calculateLrsRepetition(String substring, String lrs) {
-        if (substring.length() % lrs.length() != 0) {
-            return 1;
-        }
-
-        int repetition = substring.length() / lrs.length();
-        String repeatedLrs = lrs.repeat(repetition);
-
-        if (!repeatedLrs.equals(substring)) {
-            repetition = 1;
-        }
-
-        return repetition;
-    }
-
+class EncodeStringWithShortestLength {
     public static void main(String[] args) {
-//        String s = "abcabcdabcdabc";
-//        System.out.println(encode(s));
-
-        String s = "abbbabbbcabbbabbbc";
-        System.out.println(encode(s));
+        System.out.println(encode("abbbabbbcabbbabbbc"));
     }
+
+    // Algorithm
+    // Since no obvious greedy solution, use DP.
+    //
+    // Three cases for each recursion
+    // 1. Do nothing with the string
+    // 2. Encode the entire string once
+    // 3. Split the string into two, and combine the encodings
+    //
+    // Keep track of the min string formed. Return it at end.
+    private static final long prime = BigInteger.probablePrime(63, new Random()).longValue();
+    private static final long mod = BigInteger.probablePrime(63, new Random()).longValue();
+    private static final int radix = 256;
+
+    public static String encode(String s) {
+        return encodeRecursive(s, new HashMap<String, String>());
+    }
+
+    private static  String encodeRecursive(String s, Map<String, String> cache) {
+        if (cache.containsKey(s)) {
+            return cache.get(s);
+        }
+
+        if (s.length() < 5) {
+            cache.put(s, s);
+            return s;
+        }
+
+        // Case 2: Encode string
+        String stringEncoding = compressString(s);
+
+        // Case 3: Split into substrings
+        String minSubEncoding = "";
+        for (int i = 1; i < s.length(); i++) {
+            String left = s.substring(0, i);
+            String right = s.substring(i);
+
+            left = encodeRecursive(left, cache);
+            right = encodeRecursive(right, cache);
+
+            String encodedSubstring = left + right;
+            if (minSubEncoding.isEmpty()) {
+                minSubEncoding = encodedSubstring;
+            } else if (encodedSubstring.length() < minSubEncoding.length()) {
+                minSubEncoding = encodedSubstring;
+            }
+        }
+
+        String minString = findMinString(s, stringEncoding, minSubEncoding);
+        cache.put(s, minString);
+
+        return minString;
+    }
+
+    private static String compressString(String s) {
+        String repeatingSubstring = "";
+        int index = (s + s).indexOf(s, 1);
+        if (index >= 0) {
+            repeatingSubstring = s.substring(0, index);
+        }
+
+        int repCount = s.length() / repeatingSubstring.length();
+        String encodedString = String.format("%d[%s]", repCount, repeatingSubstring);
+
+        return encodedString.length() < s.length() ? encodedString : s;
+    }
+
+    private static  String findMinString(String s, String stringEncoding, String minSubEncoding) {
+        PriorityQueue<String> queue = new PriorityQueue<>(3, (a, b) -> a.length() - b.length());
+        queue.offer(s);
+        queue.offer(stringEncoding);
+        queue.offer(minSubEncoding);
+
+        return queue.peek();
+    }
+
+
 }
